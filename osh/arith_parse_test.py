1d3b7e1d2a5d8f687264e732055cb0a71e914d0b
Andy C <andy@chubot.org>
2016-11-19 14:46:24 -0800

    Add license headers everywhere

--- osh/arith_parse_test.py
+++ osh/arith_parse_test.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+# Copyright 2016 Andy Chu. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#   http://www.apache.org/licenses/LICENSE-2.0
 """
 arith_parse_test.py: Tests for arith_parse.py
 """
