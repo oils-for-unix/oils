f21c16023654ad24f867184df682a7b7b856ffb8
Andy Chu <andy@oilshell.org>
2017-03-03 20:50:27 -0800

    Don't pollute the repository with temp files

--- tests/redirect.test.sh
+++ tests/redirect.test.sh
@@ -21,9 +21,9 @@ echo $FOO
 # dash captures stderr to a file here, which seems correct.  Bash doesn't and
 # just lets it go to actual stderr.
 # For now we've settled on bash behavior, for compatibility?
-FOO=$(echo foo 1>&2) 2>_tmp/no-command.txt
+FOO=$(echo foo 1>&2) 2>$TMP/no-command.txt
 echo FILE=
-cat _tmp/no-command.txt
+cat $TMP/no-command.txt
 echo "FOO=$FOO"
 # OK dash/mksh stdout-json: "FILE=\nfoo\nFOO=\n"
 # stdout-json: "FILE=\nFOO=\n"
@@ -46,19 +46,19 @@ echo three 1>& 2
 ### Filename redirect with spaces
 # This time 1 *is* a descriptor, not a word.  If you add a space between 1 and
 # >, it doesn't work.
-echo two 1> _tmp/file-redir1.txt
-cat _tmp/file-redir1.txt
+echo two 1> $TMP/file-redir1.txt
+cat $TMP/file-redir1.txt
 # stdout: two
 
 ### Quoted filename redirect with spaces
 # POSIX makes node of this
-echo two \1 > _tmp/file-redir2.txt
-cat _tmp/file-redir2.txt
+echo two \1 > $TMP/file-redir2.txt
+cat $TMP/file-redir2.txt
 # stdout: two 1
 
 ### Descriptor redirect with filename
 # Should be a syntax error, but bash allows this.
-echo one 1>&nonexistent-filename__
+echo one 1>&$TMP/nonexistent-filename__
 # status: 2
 # stdout-json: ""
 # OK  mksh status: 1
@@ -68,39 +68,39 @@ echo one 1>&nonexistent-filename__
 for i in $(seq 3)
 do
   echo $i
-done > _tmp/redirect-for-loop.txt
-cat _tmp/redirect-for-loop.txt
+done > $TMP/redirect-for-loop.txt
+cat $TMP/redirect-for-loop.txt
 # stdout-json: "1\n2\n3\n"
 
 ### Prefix redirect for loop -- not allowed
->_tmp/redirect2.txt for i in $(seq 3)
+>$TMP/redirect2.txt for i in $(seq 3)
 do
   echo $i
 done
-cat _tmp/redirect2.txt
+cat $TMP/redirect2.txt
 # status: 2
 # OK mksh status: 1
 
 ### Block redirect
 # Suffix works, but prefix does NOT work.
 # That comes from '| compound_command redirect_list' in the grammar!
-{ echo block-redirect; } > _tmp/br.txt
-wc -c _tmp/br.txt
-# stdout: 15 _tmp/br.txt
+{ echo block-redirect; } > $TMP/br.txt
+cat $TMP/br.txt | wc -c
+# stdout: 15
 
 ### Redirect echo to stderr, and then redirect all of stdout somewhere.
-{ echo foo 1>&2; echo 012345789; } > _tmp/block-stdout.txt
-wc -c _tmp/block-stdout.txt
+{ echo foo 1>&2; echo 012345789; } > $TMP/block-stdout.txt
+cat $TMP/block-stdout.txt |  wc -c 
 # stderr: foo
-# stdout: 10 _tmp/block-stdout.txt
+# stdout: 10
 
 ### Redirect in the middle of two assignments
-FOO=foo >out.txt BAR=bar printenv.py FOO BAR
-tac out.txt
+FOO=foo >$TMP/out.txt BAR=bar printenv.py FOO BAR
+tac $TMP/out.txt
 # stdout-json: "bar\nfoo\n"
 
 ### Redirect in the middle of a command
-f=_tmp/out
+f=$TMP/out
 echo -n 1 2 '3 ' > $f
 echo -n 4 5 >> $f '6 '
 echo -n 7 >> $f 8 '9 '
@@ -110,9 +110,9 @@ cat $f
 # stdout-json: "1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 "
 
 ### Named file descriptor
-exec {myfd}> _tmp/named-fd.txt
+exec {myfd}> $TMP/named-fd.txt
 echo named-fd-contents >& $myfd
-cat _tmp/named-fd.txt
+cat $TMP/named-fd.txt
 # stdout: named-fd-contents
 # N-I dash/mksh stdout-json: ""
 
