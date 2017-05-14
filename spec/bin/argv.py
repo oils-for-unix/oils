66bea091d23f2ec48207c55a71b47636861f4ff9
Andy Chu <andy@oilshell.org>
2017-05-13 20:07:44 -0700

    Fix spec/bin helpers after shebang conversion.

--- spec/bin/argv.py
+++ spec/bin/argv.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from __future__ import print_function
 
 import sys
 print(sys.argv[1:])
--- spec/bin/read_from_fd.py
+++ spec/bin/read_from_fd.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from __future__ import print_function
 """
 read_from_fd.py
 """
