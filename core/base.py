1d924e8cd3aeb05cd033e7a5e8f1fb7303cfb898
Andy Chu <andy@oilshell.org>
2017-05-27 11:01:53 -0700

    Remove unused code

--- core/base.py
+++ core/base.py
@@ -3,63 +3,6 @@
 base.py -- Common base classes.
 """
 
-import io
-import sys
-
-
-# NOTE: These aren't really necessary in Python.  I'm keeping them for now
-# because I might want to have an ErrorState class that
-# {arith,bool,word,cmd}_parse.py can share.  Composition over inheritance.
-
-class _Parser(object):
-  """
-  A recursive parser needs to keep track of error messages.
-
-  fields:
-    - format string, args
-    - token number -- which can point to line and column?  Or has to be
-      computed.
-      - or is it a range of errors?
-      - maybe an error can be associated with any kind of entity: CNode,
-        ExprNode, etc. Word, WordPart.  And if there is a range of tokens, then
-        those will be highlighted.
-
-  And we also have to keep state across multiple parsers.
-
-  Do all of them have access to a arena_ / arena_ ?  For creating new objects
-  and looking them up with IDs.
-  """
-  def __init__(self, arena):
-    # The arena is used to allocate nodes and clear them all at once.  The
-    # Reader needs to allocate lines; the Lexer needs to allocate tokens.
-    self.arena = arena
-
-  def AddErrorContext(self):
-    pass
-
-  def Error(self):  # get error
-    pass
-
-
-class _Evaluator(object):
-  """
-  An recursive evaluator has to throw exceptions:
-
-    arith:        divide by zero, overflow
-    bool:         type mismatch: 1 -eq z
-    cmd executor: errexit, pipefail, etc.
-    word:         $hi'foo'"hi${}"
-                  ${var?error}
-
-  And should there be a unified stack trace system?
-  """
-  def AddErrorContext(self):
-    pass
-
-  def Error(self):  # Get error
-    pass
-
-
 class ParseError:
   """A parse error that can be formatted.
 
@@ -68,6 +11,8 @@ class ParseError:
   def __init__(self, msg, *args, **kwargs):
     self.msg = msg
     self.args = args
+    # NOTE: We use a kwargs dict because Python 2 doesn't have keyword-only
+    # args.
     self.token = kwargs.pop('token', None)
     self.word = kwargs.pop('word', None)
     if kwargs:
