8c1a6f1919653f1f822967b10fbf77dcde46b0e6
Andy Chu <andy@lisa>
2017-07-31 12:21:50 -0700

    Add missing headers to detect-readline.c
    Addresss issue #21.

--- build/detect-readline.c
+++ build/detect-readline.c
@@ -1,4 +1,8 @@
+/* 'info readline' shows an example with these three headers */
+
+#include <stdio.h>  /* readline needs this, issue #21 */
 #include <readline/readline.h>
+#include <readline/history.h>
 
 int main(void) {
   char *line = readline("");
