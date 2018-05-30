8025d46f38ebd0bbf9410d8230faa24a04a2aaec
Aleks Kamko <aykamko@gmail.com>
2018-05-30 02:51:55 +0000

    more robust readline check

--- build/detect-readline.c
+++ build/detect-readline.c
@@ -4,8 +4,12 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-int main(void) {
-  char *line = readline("");
+static int test_event_hook(void) {
   return 0;
 }
 
+int main(void) {
+  char *line = readline("");
+  rl_event_hook = test_event_hook;  // ensure readline version is recent enough
+  return 0;
+}
