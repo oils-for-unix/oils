451e0c6cbb8c2ecb4ef1d36eaa248b061007809c
Ricardo Grant <ricardo.grant1@outlook.com>
2018-07-01 23:34:56 -0400

    Update the unit test to follow the new format.
    Forgot this previously...

--- test/sh_spec_test.py
+++ test/sh_spec_test.py
@@ -10,14 +10,14 @@ import unittest
 from sh_spec import *  # module under test
 
 TEST1 = cStringIO.StringIO("""\
-### Env binding in readonly/declare disallowed
+#### Env binding in readonly/declare disallowed
 FOO=foo readonly v=$(tests/printenv.py FOO)
 echo "v=$v"
 # Shells allow this misleading construct, but the correct behavior is to
 # disallow it at parse time.
-# OK bash/dash/mksh stdout: v=None
-# OK bash/dash/mksh status: 0
-# status: 2
+## OK bash/dash/mksh stdout: v=None
+## OK bash/dash/mksh status: 0
+## status: 2
 """)
 
 TOKENS1 = list(LineIter(TEST1))
@@ -25,22 +25,22 @@ CASE1 = ParseTestCase(Tokenizer(iter(TOKENS1)))
 
 
 TEST2 = cStringIO.StringIO("""\
-### Multiline test case
+#### Multiline test case
 echo one
 echo two
-# status: 1
-# stderr-json: ""
-# STDOUT:
+## status: 1
+## stderr-json: ""
+## STDOUT:
 one
 two
-# OK dash STDOUT:
+## OK dash STDOUT:
 dash1
 dash2
-# END
-# OK mksh STDOUT:
+## END
+## OK mksh STDOUT:
 mksh1
 mksh2
-# END
+## END
 """)
 TOKENS2 = list(LineIter(TEST2))
 CASE2 = ParseTestCase(Tokenizer(iter(TOKENS2)))
