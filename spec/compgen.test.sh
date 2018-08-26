ca41519bb14d45c0bc41108a410ff7bb26ef2cb4
ricardo grant <granttrec@icould.com>
2018-08-26 03:35:11 +0000

    Update compgen test to check alphabetical output order

--- spec/compgen.test.sh
+++ spec/compgen.test.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 #### Print function list
-env -i /bin/bash --norc --noprofile -c 'add () { expr 4 + 4; }; \
-                                        div () { expr 6 / 2; }; \
-                                        ek () { echo hello;} \
+env -i /bin/bash --norc --noprofile -c 'add () { expr 4 + 4; } \
+                                        && div () { expr 6 / 2; } \
+                                        && ek () { echo hello; } \
+                                        && __ec () { echo hi; } \
+                                        && _ab () { expr 10 % 3; } \
                                         && compgen -A function'
 ## status: 0
 ## STDOUT:
+__ec
+_ab
 add
 div
 ek
