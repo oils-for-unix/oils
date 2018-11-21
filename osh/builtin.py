0d25ecd0efc5e17d3bf39051e67ae519cf575246
Andy Chu <andy@oilshell.org>
2018-11-21 09:53:54 -0800

    [release] Temporarily disable printf for release.
    See issue #211.
    
    Also fixed the build manifest to account for ASDL renaming.

--- osh/builtin.py
+++ osh/builtin.py
@@ -85,7 +85,8 @@ _SPECIAL_BUILTINS = {
 _NORMAL_BUILTINS = {
     "read": builtin_e.READ,
     "echo": builtin_e.ECHO,
-    "printf": builtin_e.PRINTF,
+    # TODO: re-enable after release
+    #"printf": builtin_e.PRINTF,
     "cd": builtin_e.CD,
     "pushd": builtin_e.PUSHD,
     "popd": builtin_e.POPD,
