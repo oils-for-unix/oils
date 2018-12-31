372de9efdd8816e91594cb147b44482a0e00280a
Andy Chu <andy@oilshell.org>
2018-12-31 11:34:01 -0800

    [demo] Clean up demo/pyreadline.py.
    This is a nice example of using a completion callback and a candidate
    display callback.

--- demo/pyreadline.py
+++ demo/pyreadline.py
@@ -14,63 +14,38 @@ def log(msg, *args):
   print(msg, file=sys.stderr)
 
 
-USE_DELIMS = False
-#USE_DELIMS = True
-
-
 def Words(prefix):
   #log('Words')
   words = ['foo', 'bar', 'baz', 'spam', 'eggs']
   first = 'echo '
   #first = 'echo\\ '  # Test quoting
 
-  if USE_DELIMS:
-    pass
-  else:
-    words = [first + w for w in words]
+  candidates = [first + w for w in words]
 
   #log('words = %s', words)
-  for w in words:
-    if w.startswith(prefix):
-      yield w + ' '
+  for c in candidates:
+    if c.startswith(prefix):
+      yield c + ' '
 
 
-class Callback(object):
-  def __init__(self, comp):
-    self.comp = comp
+class CompletionCallback(object):
+  def __init__(self, comp_state):
+    self.comp_state = comp_state
     self.iter = None
 
-  # Another idea:
-  # Take word_prefix and then slice the whole line?
-  # Then parse it?
-
-  # First, Rest, VarName
-  # Tilde Sub too
-
   def Call(self, word_prefix, state):
+    """Generate completions."""
 
     #log('Called with %r %r', word_prefix, state)
-
-    if state == 0:
-      self.comp['ORIG'] = readline.get_line_buffer()
-
-      # the whole thing
-      # get_line_buffer()
-      #
-      # # parsed
-      # get_begidx()  # parsed
-      # get_endidx()  # parsed
-      #
-      # But how do we REPLACE a word
-
+    if state == 0:  # initial completion
+      # Save for later
+      self.comp_state['ORIG'] = readline.get_line_buffer()
       self.iter = Words(word_prefix)
-      #log('init iter %s', self.iter)
 
     try:
       c = self.iter.next()
     except StopIteration:
       c = None
-    #log('-> %r', c)
     return c
 
   def __call__(self, word_prefix, state):
@@ -82,16 +57,29 @@ class Callback(object):
       raise
 
 
-# Hm this hook is useful!  I can strip off the common prefix?
-# And I can display flag help?
-# Everything is a string, but I can look up help with strings.  Hm this is
-# good.
-#
-# Problem: how do I detect where the bottom of the screen is?
+class DisplayCallback(object):
+  """Hook to display completion candidates.
 
-class Display(object):
-  def __init__(self, comp):
-    self.comp = comp
+  This is useful for:
+  - stripping off the common prefix according to OUR rules
+  - display builtin and flag help
+
+  Problem: how do I detect where the bottom of the screen is?
+
+  Features here:
+  - TODO: query terminal width
+  - limit the number of matches to 10 or so?
+    - so then do you need an option to set this limit?  COMPLIMIT?
+    - what does readline do?
+  """
+
+  def __init__(self, comp_state, max_lines=5):
+    self.comp_state = comp_state
+    self.max_lines = max_lines  # TODO: Respect this!
+    self.num_lines_last_displayed = 0
+
+  def Reset(self):
+    """Call this in between commands."""
     self.num_lines_last_displayed = 0
 
   def __call__(self, subst, matches, longest_match_len):
@@ -103,7 +91,7 @@ class Display(object):
     print('')
 
     # Have to delete previous completions!
-    ClearLines(self.num_lines_last_displayed)
+    EraseLines(self.num_lines_last_displayed)
 
     # Print and go back up.  But we have to ERASE these before hitting enter!
     for m in matches:
@@ -120,21 +108,16 @@ class Display(object):
     #sys.stdout.write('\x1b[%dC' % 4)  # RIGHT
     #sys.stdout.write('\x1b[%dD' % 1)  # RIGHT by prompt
 
-    # PROMPT again
-    sys.stdout.write('$ ')
-    sys.stdout.write(self.comp['ORIG'])
-
-    # Features here:
-    # - TODO: query terminal width
-    # - strip common prefixes?  It has to be a whole line?
-    #   - maybe you need a hack to associate the unique whole line with
-    #     some kind of initial position?
-    # - limit the number of matches to 10 or so?
-    #   - so then do you need an option to set this limit?  COMPLIMIT?
-    #   - what does readline do?
+    if 0:
+      # PROMPT again
+      sys.stdout.write('$ ')
+      sys.stdout.write(self.comp_state['ORIG'])
+    else:
+      n = len(self.comp_state['ORIG']) + 2  # length of prompt
+      sys.stdout.write('\x1b[%dC' % n)  # RIGHT
 
 
-def ClearLines(n):
+def EraseLines(n):
   if n == 0:
     return
 
@@ -146,25 +129,15 @@ def ClearLines(n):
   sys.stdout.write('\x1b[%dA' % (n))
 
 
-# What bash and OSH use.  Should OSH use something else?
-READLINE_DELIMS = ' \t\n"\'><=;|&(:'
-
 def main(argv):
-  # Unit test
+  comp_state = {}
+  readline.set_completer(CompletionCallback(comp_state))
 
-  comp = {}
-  readline.set_completer(Callback(comp))
+  # If we do this, we get the whole line.  Then we need to use DisplayCallback
+  # to get it.
+  readline.set_completer_delims('')
 
-  # OK, if we do this, then we get the whole damn line!!!
-  # Then we just return the whole damn line completed?
-  # Problem : the displayed completions.  Is there an option to strip
-  # common prefix?
-  if USE_DELIMS:
-    readline.set_completer_delims(READLINE_DELIMS)
-  else:
-    readline.set_completer_delims('')
-
-  hook = Display(comp)
+  hook = DisplayCallback(comp_state)
   readline.set_completion_display_matches_hook(hook)
 
   readline.parse_and_bind('tab: complete')
@@ -174,8 +147,9 @@ def main(argv):
     except EOFError:
       print()
       break
-    # Clear lines before printing
-    ClearLines(hook.num_lines_last_displayed)
+    # Erase lines before execution
+    EraseLines(hook.num_lines_last_displayed)
+    hook.Reset()
     print(x)
 
 
@@ -185,4 +159,3 @@ if __name__ == '__main__':
   except RuntimeError as e:
     print('FATAL: %s' % e, file=sys.stderr)
     sys.exit(1)
-
