4aa303af92d721005e6de2e33f567e20aef19337
Andy Chu <andy@oilshell.org>
2019-02-16 01:57:49 -0800

    [cleanup] Remove old ASDL -> Python code generator.

--- core/asdl_gen.py
+++ core/asdl_gen.py
@@ -5,7 +5,6 @@ asdl_gen.py - Generate Python and C from OSH ASDL schemas.
 from __future__ import print_function
 
 import os
-import pickle
 import sys
 
 from asdl import front_end
@@ -13,7 +12,7 @@ from asdl import gen_cpp
 from asdl import gen_python
 from asdl import meta
 
-from core.util import log
+#from core.util import log
 
 def main(argv):
   try:
@@ -26,7 +25,6 @@ def main(argv):
   except IndexError:
     raise RuntimeError('Schema path required')
 
-  # To avoid circular dependencies, don't load Id for types.asdl.
   if os.path.basename(schema_path) == 'types.asdl':
     app_types = {}
   else:
@@ -38,38 +36,6 @@ def main(argv):
 
     v = gen_cpp.CEnumVisitor(sys.stdout)
     v.VisitModule(schema_ast)
-
-  elif action == 'py':  # Generate Python code so we don't depend on ASDL schemas
-    pickle_out_path = argv[3]
-
-    with open(schema_path) as f:
-      schema_ast, type_lookup = front_end.LoadSchema(f, app_types)
-
-    f = sys.stdout
-
-    f.write("""\
-from asdl import const  # For const.NO_INTEGER
-from asdl import runtime
-from pylib import unpickle
-
-from core import util
-
-f = util.GetResourceLoader().open('%s')
-TYPE_LOOKUP = unpickle.load_v2_subset(f)
-f.close()
-
-""" % pickle_out_path)
-
-    v = gen_python.GenClassesVisitor(f)
-    v.VisitModule(schema_ast)
-
-    if pickle_out_path:
-      # Pickle version 2 is better.  (Pickle version 0 uses
-      # s.decode('string-escape')! )
-      # In version 2, now I have 16 opcodes + STOP.
-      with open(pickle_out_path, 'w') as f:
-        pickle.dump(type_lookup, f, protocol=2)
-      log('Wrote %s', pickle_out_path)
 
   elif action == 'mypy':  # typed mypy
     with open(schema_path) as f:
