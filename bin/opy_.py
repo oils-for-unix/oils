65dd46c7794b54c07a4a1f4f58c5320d5432cd73
Andy Chu <andy@oilshell.org>
2019-03-20 21:53:57 -0700

    Make bin/{opy,opyc} consistent with the bin/oil wrappers.
    We now rely on 'typing' so we need PYTHONPATH to include
    $REPO_ROOT/vendor.

--- bin/opy_.py
+++ bin/opy_.py
@@ -7,9 +7,6 @@ opy_.py
 import os
 import sys
 
-this_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-sys.path.append(os.path.join(this_dir, '..'))
-
 from frontend import args
 from core import pyutil
 from core import util
--- misc/bin.sh
+++ misc/bin.sh
@@ -9,7 +9,7 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
-source build/common.sh  # for OIL_SYMLINKS
+source build/common.sh  # for OIL_SYMLINKS and OPY_SYMLINKS
 
 # For osh-dbg.
 ovm-snippet() {
@@ -27,11 +27,18 @@ make-osh-dbg() {
 
 # A snippet that sets PYTHONPATH for bin/oil.py and runs it with the right
 # action.
-dev-snippet() {
+oil-dev-snippet() {
   local name=$1
   echo '#!/bin/sh'
   echo 'REPO_ROOT=$(cd $(dirname $(dirname $0)) && pwd)'
   echo 'PYTHONPATH=$REPO_ROOT:$REPO_ROOT/vendor exec $REPO_ROOT/bin/oil.py '$name' "$@"'
+}
+
+opy-dev-snippet() {
+  local name=$1
+  echo '#!/bin/sh'
+  echo 'REPO_ROOT=$(cd $(dirname $(dirname $0)) && pwd)'
+  echo 'PYTHONPATH=$REPO_ROOT:$REPO_ROOT/vendor exec $REPO_ROOT/bin/opy_.py '$name' "$@"'
 }
 
 make-bin-links() {
@@ -40,18 +47,21 @@ make-bin-links() {
   mkdir -p bin _bin
 
   for link in "${OIL_SYMLINKS[@]}"; do
-    dev-snippet $link > bin/$link
+    # bin/ shell wrapper
+    oil-dev-snippet $link > bin/$link
     chmod +x bin/$link
     echo "Wrote bin/$link"
-  done
 
-  for link in "${OIL_SYMLINKS[@]}"; do
+    # _bin/ symlink
     ln -s -f --verbose oil.ovm _bin/$link
   done
 
-
   for link in "${OPY_SYMLINKS[@]}"; do
-    ln -s -f --verbose opy_.py bin/$link
+    opy-dev-snippet $link > bin/$link
+    chmod +x bin/$link
+    echo "Wrote bin/$link"
+
+    # _bin/ symlink
     ln -s -f --verbose opy.ovm _bin/$link
   done
 
