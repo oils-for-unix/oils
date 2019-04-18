72c0aadcbc6d48416ba63a12092e17e42e2439bd
Andy Chu <andy@oilshell.org>
2019-04-18 12:34:54 -0700

    [mycpp] Brainstorming on typeswitch vs. switch.

--- mycpp/examples/switch1.py
+++ mycpp/examples/switch1.py
@@ -17,6 +17,8 @@ from typing import List, Any
 
 # This could go in pyutil.py or something
 class Switch(object):
+  """A ContextManager that translates to a C switch statement."""
+
   def __init__(self, value):
     # type: (int) -> None
     self.value = value
@@ -32,6 +34,24 @@ class Switch(object):
   def __call__(self, *values):
     # type: (*Any) -> bool
     return self.value in values
+
+
+
+class typeswitch(object):
+  """A ContextManager that translates to a C switch statement.
+  
+  Each case will have a static_cast<>.  It does nothing at runtime."""
+  def __init__(self):
+    # type: (int) -> None
+    pass
+
+  def __enter__(self):
+    # type: () -> None
+    pass
+
+  def __exit__(self, type, value, traceback):
+    # type: (Any, Any, Any) -> bool
+    return False  # Allows a traceback to occur
 
 
 def run_tests():
@@ -76,6 +96,43 @@ def run_tests():
 
     else:
       print('default')
+
+  # TODO: type switch will be more common.  We need to preserve instance() for
+  # MyPy.
+  # 'typeswitch' involves CASTING, while 'switch' does not.  'switch' is for
+  # simple sum types (enums).
+  # 
+  # with typeswitch(node) as _:  # dummy for translation
+  #   if isinstance(node, expr__Const):
+  #     n = node                        # rename for casting?
+  #     return n.i
+  #
+  #   elif isinstance(node, expr__Var):
+  #     return self.mem[node.name]
+  #
+  #   elif isinstance(node, expr__Binary):
+  #     return self.Eval(node.left) + self.Eval(node.right)
+  #
+  # switch (node.tag) {
+  #   case expr_e::Const: {
+  #     n = static_cast<expr__Const>(node);
+  #     return n.i
+  #   }
+  #   case expr_e::Var: {
+  #     n = static_cast<expr__Var>(node);
+  #     return this->mem_[n.name]
+  #   }
+  # }
+
+  node = 3
+  #node = True
+  with typeswitch() as _:
+    if isinstance(node, bool):
+      n = node
+      log('bool %s', n)
+    elif isinstance(node, int):
+      n = node
+      log('int %s', n)
 
 
 def run_benchmarks():
