a0a56724cfbecffb26aa29e0f5cddebe69bc548e
Andy Chu <andy@oilshell.org>
2019-06-04 01:09:51 -0700

    [build] Build grammar tables on Travis too.

--- build/dev.sh
+++ build/dev.sh
@@ -184,6 +184,9 @@ minimal() {
   asdl/run.sh gen-typed-demo-asdl
   asdl/run.sh gen-typed-arith-asdl
 
+  # Needed on Travis.
+  grammar
+
   pylibc
   line-input
 }
@@ -196,7 +199,6 @@ grammar() {
 all() {
   minimal
   fastlex
-  grammar
 }
 
 "$@"
