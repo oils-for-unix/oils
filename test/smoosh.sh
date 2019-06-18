9d2c6519240d72ea378a3fa102db8bae388b4efc
Andy Chu <andy@oilshell.org>
2019-06-18 11:26:10 -0700

    [test/smoosh] Some progress on building it.
    Still having problems with the zarith dependency.

--- test/smoosh.sh
+++ test/smoosh.sh
@@ -9,9 +9,22 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
-clone() {
-  cd ~/git/languages
-  git clone https://github.com/mgree/smoosh
+readonly OPAM=~/.opam
+
+# Copies of lem we built inside the repo.
+export PATH="$PWD/lem/bin:${PATH}"
+export LEMLIB="$PWD/lem/library"
+
+
+# IMPORTANT: Every function uses this OCaml environment!
+
+eval $(opam config env)
+
+
+update() {
+  #cd ~/git/languages
+  #git clone https://github.com/mgree/smoosh
+  git pull
   git submodule update --init
 }
 
@@ -23,6 +36,15 @@ install-system-deps() {
   sudo apt install opam
 }
 
+opam-switch() {
+  # list compilers:
+  # opam switch list
+
+  # Version that upstream developers use.  4.02 is the system compiler on
+  # Ubuntu 16.04, but that's too old (Sys.int_size not defined)
+  opam switch 4.07.0
+}
+
 # NOTE: I didn't have opam modify .bashrc.  So from now on it's:
 #
 # eval $(opam config env)
@@ -30,8 +52,6 @@ install-system-deps() {
 # State is in ~/.opam
 
 install-ocaml-deps() {
-  eval $(opam config env)
-
   # make sure we have ocamlfind and ocamlbuild
   opam install ocamlfind ocamlbuild
 
@@ -48,38 +68,29 @@ link-this-script() {
   ln -s -f -v $this_script ~/git/languages/smoosh
 }
 
-readonly OPAM=~/.opam
 
-# Copies of lem we built inside the repo.
-export PATH="$PWD/lem/bin:${PATH}"
-export LEMLIB="$PWD/lem/library"
-
-_deps1() {
-  eval $(opam config env)
-
+deps1() {
   # zarith already installed error?
+
+  # Why is this being installed with the system compiler?
+  # I have to do "opam switch" as root?
+
   pushd lem/ocaml-lib 
-  opam config exec -- make install_dependencies
+  opam config exec -- sudo make install_dependencies
   popd
 }
 
-# NOTE: Had to wipe out ~/.opam, do this first, and then do it over?
-# Need root to install
-deps1() {
-  sudo $0 _deps1 "$@"
-}
-
-repo-deps() {
+build-lem() {
   set -x
-
-  eval $(opam config env)
 
   # set up lem
   pushd lem 
   opam config exec -- make
   opam config exec -- make install
   popd
+}
 
+build-libdash() {
   # build libdash, expose shared object
   pushd libdash
   ./autogen.sh && ./configure --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu
