06cca17f9b15a3196a490d21d03458c76d10bfb3
Andy Chu <andy@oilshell.org>
2019-07-04 13:17:09 -0700

    [demo] Show that OSH is calling execve() too much.
    Related to #355.

---
+++ demo/path-cache.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Test how many calls to execve() there are.
+# This is also a good demo of extraneous stat() calls at startup!  bash has a
+# bunch.
+#
+# Usage:
+#   ./path-cache.sh <function name>
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+sh-demo() {
+  local sh=$1
+  local syscalls='execve,stat,lstat'
+  #local syscalls='execve'
+  strace -ff -e $syscalls  -- $sh -c 'whoami; whoami'
+}
+
+main() {
+  for sh in dash bash bin/osh; do
+    echo $'\t---'
+    echo $'\t'$sh
+    echo $'\t---'
+
+    sh-demo $sh
+  done
+}
+
+"$@"
