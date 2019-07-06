2138fed0b27e9ca746224b6062d4e0fa8b266ba5
Joshua Nelson <jyn514@gmail.com>
2019-07-06 13:22:33 -0400

    Handle -1 from wcwidth
    Only try to find width of last line of prompt.
    Handle -1 from wcwidth by returning number of bytes in prompt.
    Does not handle newline inside of escape characters.
    
    Addresses https://github.com/oilshell/oil/issues/394

--- core/comp_ui.py
+++ core/comp_ui.py
@@ -32,10 +32,12 @@ PROMPT_REVERSE = '\x01%s\x02' % _REVERSE
 def _PromptLen(prompt_str):
   """Ignore all characters between \x01 and \x02 and handle unicode characters.
   In particular, the display width of a string may be different from either the
-  number of bytes or the number of unicode characters."""
+  number of bytes or the number of unicode characters.
+  Additionally, if there are multiple lines in the prompt, only give the length of the last line."""
   escaped = False
   display_str = ""
-  for c in prompt_str:
+  last_line = prompt_str.split('\n')[-1]
+  for c in last_line:
     if c == '\x01':
       escaped = True
     elif c == '\x02':
@@ -43,7 +45,10 @@ def _PromptLen(prompt_str):
     elif not escaped:
       display_str += c
   try:
-    return libc.wcswidth(display_str)
+    width = libc.wcswidth(display_str)
+    if width == -1:
+        return len(display_str)
+    return width
   # en_US.UTF-8 locale missing, just return the number of bytes
   except (SystemError, UnicodeError):
     return len(display_str)
--- core/comp_ui_test.py
+++ core/comp_ui_test.py
@@ -165,6 +165,10 @@ class PromptTest(unittest.TestCase):
         comp_ui._PromptLen("\x01\x02 hi \x01hi\x02 \x01\x02 hello"),
         len(" hi   hello"))
 
+  def testNewline(self):
+    self.assertEqual(comp_ui._PromptLen("\n"), 0)
+    self.assertEqual(comp_ui._PromptLen("abc\ndef"), 3)
+    self.assertEqual(comp_ui._PromptLen(""), 0)
 
 if __name__ == '__main__':
   unittest.main()
