aecfe9f82e251a86e4e4b891e03630f814b883bd
Joshua Nelson <jyn514@gmail.com>
2019-07-20 08:42:31 -0400

    Fix style issues with demo

--- demo/read-n-demo.sh
+++ demo/read-n-demo.sh
@@ -2,22 +2,24 @@
 set -euo pipefail
 
 read-one () {
-	# try typing a letter without hitting newline
-	# the shell should respond immediately
-	read -n 1
-	echo $'\n'"$REPLY"
+  # try typing a letter without hitting newline
+  # the shell should respond immediately
+  read -n 1
+  echo
+  echo "$REPLY"
 }
 
 read-multiple () {
-	read -n 5
-	echo $'\n'"$REPLY"
+  read -n 5
+  echo
+  echo "$REPLY"
 }
 
 all () {
-	echo "one char:"
-	read-one
-	echo "multiple chars:"
-	read-multiple
+  echo "one char:"
+  read-one
+  echo "multiple chars:"
+  read-multiple
 }
 
 "$@"
