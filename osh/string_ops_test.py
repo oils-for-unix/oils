0b9445adea807545bb1bc8f49ea5424db78c0c26
Aaron Sokoloski <aaron@mustardsystems.com>
2019-10-30 22:40:10 -0500

    [test] add tests for _NextUtf8Char and _PreviousUtf8Char

--- osh/string_ops_test.py
+++ osh/string_ops_test.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 
 import unittest
 
+from core import util
 from osh import string_ops  # module under test
 
 
@@ -25,6 +26,55 @@ class LibStrTest(unittest.TestCase):
       print('')
       print('Utf8Encode case %r %r' % (expected, code_point))
       self.assertEqual(expected, string_ops.Utf8Encode(code_point))
+
+  def test_NextUtf8Char(self):
+    CASES = [
+        ([1, 3, 6, 10], '\x24\xC2\xA2\xE0\xA4\xB9\xF0\x90\x8D\x88'),
+        ([1, 3, 'Invalid UTF-8 continuation byte'], '\x24\xC2\xA2\xE0\xE0\xA4'),
+        ([1, 3, 6, 'Invalid start of UTF-8 character'], '\x24\xC2\xA2\xE0\xA4\xA4\xB9'),
+        ([1, 3, 'Invalid start of UTF-8 character'], '\x24\xC2\xA2\xFF'),
+        ([1, 'Incomplete UTF-8 character'], '\x24\xF0\x90\x8D'),
+    ]
+    for expected_indexes, input_str in CASES:
+      print()
+      print('_NextUtf8Char case %r %r' % (expected_indexes, input_str))
+      i = 0
+      actual_indexes = []
+      while True:
+        try:
+          i = string_ops._NextUtf8Char(input_str, i)
+          actual_indexes.append(i)
+          if i >= len(input_str):
+            break
+        except util.InvalidUtf8 as e:
+          actual_indexes.append(e.msg)
+          break
+      self.assertEqual(expected_indexes, actual_indexes)
+
+  def test_PreviousUtf8Char(self):
+    # The error messages could probably be improved for more consistency
+    # with NextUtf8Char, at the expense of more complexity.
+    CASES = [
+        ([6, 3, 1, 0], '\x24\xC2\xA2\xE0\xA4\xB9\xF0\x90\x8D\x88'),
+        ([6, 3, 1, 'Invalid start of UTF-8 character'], '\xA2\xC2\xA2\xE0\xA4\xB9\xF0\x90\x8D\x88'),
+        ([10, 'Invalid start of UTF-8 character'], '\xF0\x90\x8D\x88\x90\x8D\x88\x90\x8D\x88\x24'),
+        ([3, 'Invalid start of UTF-8 character'], '\xF0\x90\x8D\x24'),
+    ]
+    for expected_indexes, input_str in CASES:
+      print()
+      print('_PreviousUtf8Char case %r %r' % (expected_indexes, input_str))
+      i = len(input_str)
+      actual_indexes = []
+      while True:
+        try:
+          i = string_ops._PreviousUtf8Char(input_str, i)
+          actual_indexes.append(i)
+          if i == 0:
+            break
+        except util.InvalidUtf8 as e:
+          actual_indexes.append(e.msg)
+          break
+      self.assertEqual(expected_indexes, actual_indexes)
 
   def testUnarySuffixOpDemo(self):
     print(string_ops)
