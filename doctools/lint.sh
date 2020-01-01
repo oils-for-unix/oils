0122d781542bb5d81f127b310f8fee980c40435a
Andy Chu <andy@oilshell.org>
2020-01-01 15:55:57 -0800

    [css] Remove leading indent from <ul> and <ol>.
    This has been bugging me for a long time!
    
    Also add a shell function to quickly build HTML for previewing style (on
    the server.)

--- doctools/lint.sh
+++ doctools/lint.sh
@@ -7,6 +7,14 @@ set -o nounset
 set -o pipefail
 set -o errexit
 
+#
+# TODO: Use this 2017 release!
+# 
+# http://www.html-tidy.org/
+# http://binaries.html-tidy.org/
+#
+# The Ubuntu version is from 2009!
+
 validate-html() {
   # -e shows only errors
   # -q suppresses other text
@@ -26,18 +34,60 @@ validate-html() {
 }
 
 manifest() {
-  find \
-    _release/VERSION _tmp/unit _tmp/spec \
-    -name '*.html' 
-    # There are a lot of empty <pre></pre> here which I don't care about
-    # _tmp/spec \
-    #_tmp/test-opy _tmp/metrics \ 
+  local -a html_dirs=(
+    _release/VERSION 
+    _tmp/unit
+    #_tmp/spec
+    _tmp/osh-parser
 
-  # TODO: include benchmarks.  Look at devtools/release.sh compress
+    #_tmp/test-opy _tmp/metrics \ 
+  )
+  # There are a lot of empty <pre></pre> here which I don't care about
+  # _tmp/spec \
+  # 
+  # things I don't care about:
+  # - empty <pre> resulting from auto-generated code
+  # - proprietary attributes -- I use some, doesn't understand "col"
+
+  find "${html_dirs[@]}" -name '*.html' 
 }
 
-all-html() {
+release-tree() {
+  ### Lint SOME of the release tree
+
   manifest | xargs -n 1 --verbose -- $0 validate-html
+}
+
+# A tree of files to test.
+
+# Note: you also need to look at the originals in _tmp/unit, etc. because most
+# of these are compressed.
+
+quickly-build-release-tree() {
+  test/unit.sh write-report
+  test/wild-runner.sh make-report
+
+  local suite='osh'
+  local manifest=_tmp/spec/SUITE-$suite.txt
+
+  test/spec-runner.sh all-tests-to-html $manifest
+  test/spec-runner.sh html-summary $suite
+
+  local version=0.7.pre10
+
+  devtools/release-version.sh git-changelog-$version
+  devtools/release-version.sh announcement-$version
+
+  benchmarks/report.sh all
+
+  devtools/release.sh build-tree
+
+  # gah this builds .wwz files
+  # but it shows you all the locations where they live
+  devtools/release.sh compress
+
+  # Now to ./local.sh test-release-tree to upload _release/VERSION to the
+  # server
 }
 
 "$@"
