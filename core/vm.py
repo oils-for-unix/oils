4a62fa1a92456fce34dc942de479dd919dba5b9c
Andy Chu <andy@oilshell.org>
2020-02-03 19:26:49 -0800

    [types] Annotate the new core/vm.py file.

--- core/vm.py
+++ core/vm.py
@@ -8,15 +8,15 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
   from osh.sh_expr_eval import ArithEvaluator
   from osh.sh_expr_eval import BoolEvaluator
-  from oil_lang.expr_eval import OilEvalutor
-  from osh.word_eval import _WordEvaluator
+  from oil_lang.expr_eval import OilEvaluator
+  from osh.word_eval import NormalWordEvaluator
   from osh.cmd_exec import Executor
   from osh import prompt
   from core import dev
 
 
 def InitCircularDeps(arith_ev, bool_ev, expr_ev, word_ev, ex, prompt_ev, tracer):
-  # type: (ArithEvaluator, BoolEvaluator, OilEvalutor, _WordEvaluator, Executor, prompt.Evaluator, dev.Tracer) -> None
+  # type: (ArithEvaluator, BoolEvaluator, OilEvaluator, NormalWordEvaluator, Executor, prompt.Evaluator, dev.Tracer) -> None
   arith_ev.word_ev = word_ev
   bool_ev.word_ev = word_ev
 
