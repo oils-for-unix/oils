448b95d82b878d458518d833caea2745cce41ac0
Andy Chu <andy@oilshell.org>
2020-02-10 15:43:20 -0800

    [fix] Revert accidental renamings from last commit.

--- osh/word_parse.py
+++ osh/word_parse.py
@@ -1247,7 +1247,7 @@ class WordParser(WordEmitter):
           # Look ahead for $strfunc(x)
           #   $f(x) or --name=$f(x) is allowed
           #   but "--name=$f(x)" not allowed?  This would BREAK EXISTING CODE.
-          #   It would need a parse opt_num.
+          #   It would need a parse option.
 
           next_id = self.lexer.LookAhead(lex_mode_e.ShCommand)
           if next_id == Id.Op_LParen:
