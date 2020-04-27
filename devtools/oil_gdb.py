df47b087e490d3e5ea52976def904ff77f4eee81
Andy Chu <andy@oilshell.org>
2020-04-26 21:57:46 -0700

    [devtools] GDB plugin to print mylib Str* and an ASDL value!
    We need to generate the GDB plugin from the ASDL schemas!  Interesting.

--- devtools/oil_gdb.py
+++ devtools/oil_gdb.py
@@ -19,3 +19,88 @@ class SimpleCommand(gdb.Command):
 
 # This registers our class to the gdb runtime at "source" time.
 SimpleCommand()
+
+
+# Following:
+# https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/6/html/developer_guide/debuggingprettyprinters
+
+class StrPrinter:
+    def __init__(self, val):
+        self.val = val
+
+    def to_string(self):
+        len_ = self.val['len_']
+
+        # This is a gdb.Value object
+        data_ = self.val['data_']
+        
+        # Make a lazystring out of it
+        # https://sourceware.org/gdb/current/onlinedocs/gdb/Values-From-Inferior.html
+        # TODO: could try utf-8 too
+        return data_.lazy_string('ascii', len_)
+
+class value__Printer:
+    """
+    runtime_asdl::value_t
+
+    _value_str = {
+      1: 'value.Undef',
+      2: 'value.Str',
+      3: 'value.Int',
+      4: 'value.MaybeStrArray',
+      5: 'value.AssocArray',
+      6: 'value.Eggex',
+      7: 'value.Obj',
+    }
+    """
+    def __init__(self, val):
+        self.val = val
+
+    def to_string(self):
+        # TODO: copy below
+        pass
+
+
+class part_value__Printer:
+    """
+    _part_value_str = {
+      1: 'part_value.String',
+      2: 'part_value.Array',
+    }
+    """
+    def __init__(self, val):
+        self.val = val
+
+    def to_string(self):
+        # Get address of value and look at first 16 bits
+
+        obj = self.val.dereference()  # location of part_value_t
+
+        # read the uint16_t tag
+        tag_mem = gdb.selected_inferior().read_memory(obj.address, 2)
+
+        # it's little endian
+        tag = ord(tag_mem[0])
+        #print('tag %r' % tag)
+
+        if tag == 1:
+          typ = gdb.lookup_type('runtime_asdl::part_value__String').pointer()
+        elif tag == 2:
+          typ = gdb.lookup_type('runtime_asdl::part_value__Array').pointer()
+        else:
+          raise AssertionError('Invalid tag %d', tag)
+
+        v = self.val.cast(typ)
+        return v.dereference()
+
+
+def lookup_type(val):
+    if str(val.type) == 'Str *':
+        return StrPrinter(val)
+    #if str(val.type) == 'runtime_asdl::value_t *':
+    #    return value__Printer(val)
+    if str(val.type) == 'runtime_asdl::part_value_t *':
+        return part_value__Printer(val)
+    return None
+
+gdb.pretty_printers.append(lookup_type)
