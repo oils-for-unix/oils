76c9bce6560627e28468a5c071a6cea1c71dfe44
Andy Chu <andy@oilshell.org>
2020-11-11 19:58:30 -0800

    [build/oil-defs] Add missing selectmodule.c
    Hm were the last few releases missing this?

--- stdlib/oil-polyfill.sh
+++ stdlib/oil-polyfill.sh
@@ -36,3 +36,6 @@ die() {
 # ssh $USER@$HOST "$(printf '%q ' "$@")"
 #
 # Also need one for scp.
+
+# Idea: from tea/run.sh, exit 255 for xargs
+# Does this need run --assign-status, or can it be POSIX shell?
