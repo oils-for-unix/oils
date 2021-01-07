f95a61c7fd3693878b8d8f4efa26d1a569e63752
Andy Chu <andy@oilshell.org>
2021-01-07 10:21:22 -0800

    [mycpp] Raise heap size temporarily to disable GC.
    See if the translation still works.

--- mycpp/harness.sh
+++ mycpp/harness.sh
@@ -8,10 +8,9 @@ gen-main() {
   cat <<EOF
 
 int main(int argc, char **argv) {
-  // gc_heap::gHeap.Init(1 << 10);  // for debugging
-  gc_heap::gHeap.Init(512);
+  // gc_heap::gHeap.Init(512);
   // gc_heap::gHeap.Init(128 << 10);  // 128 KiB; doubling in size
-  // gc_heap::gHeap.Init(400 << 20);  // 400 MiB to avoid garbage collection
+  gc_heap::gHeap.Init(400 << 20);  // 400 MiB to avoid garbage collection
 
   if (getenv("BENCHMARK")) {
     fprintf(stderr, "Benchmarking...\n");
