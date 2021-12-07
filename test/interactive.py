28473609723a653a6d1ff463a623daffa9884fa2
emdash <dotsony@gmail.com>
2021-12-07 10:01:23 -0800

    Make the interactive test script friendlier: - doc strings instead of comments, where approprate - configurable via env vars - debug mode disabled by default - friendlier output - exit code indicates number of failing tests - Make shell argument to InteractiveTest optional

--- test/interactive.py
+++ test/interactive.py
@@ -6,42 +6,91 @@ import signal
 import sys
 import os
 
-SHELL = "bin/osh"
-DEBUG = True
+
+SHELL = os.environ.get("OSH_TEST_INTERACTIVE_SHELL", "bin/osh")
+DEBUG = "OSH_TEST_INTERACTIVE_DEBUG" in os.environ
+TIMEOUT = int(os.environ.get("OSH_TEST_INTERACTIVE_TIMEOUT", 2))
+
+global g_failures
+g_failures = 0
+
 
 def get_pid_by_name(name):
-    # XXX: make sure this is restricted this to subprocesses under us.
+    """Return the pid of the process matching `name`."""
+    # XXX: make sure this is restricted to subprocesses under us.
     return int(pexpect.run("pgrep cat").split()[-1])
 
+
+# XXX: osh.sendcontrol("z") does not suspend the foreground process :(
+#
+# why does osh.sendcontrol("c") generate SIGINT, while osh.sendcontrol("z")
+# appears to do nothing?
 def stop_process__hack(name):
-    # calling osh.sendcontrol("z") does not suspend the foreground process :(
-    # find the process running under us one and send SIGSTOP directly.
+    """Send sigstop to the most recent process matching `name`"""
     os.kill(get_pid_by_name(name), signal.SIGSTOP)
 
-def kill_process__hack(name):
+
+def kill_process(name):
+    """Kill the most recent process matching `name`."""
     os.kill(get_pid_by_name(name), signal.SIGINT)
 
 
-# Context manager wrapper around pexpect.spawn
-class TestShell(object):
+class InteractiveTest(object):
+    """Define a test case for the interactive shell.
 
-    def __init__(self, cmdline):
+    This is implemented as a context-manager wrapper around
+    pexpect.spawn, used like:
+
+    with InteractiveTest(....) as osh:
+       osh.sendline(...)
+       osh.expect(...)
+       ...
+    """
+
+    def __init__(self, description, cmdline=SHELL):
         self.cmdline = cmdline
         self.shell = None
+        self.description = description
 
     def __enter__(self):
-        self.shell = pexpect.spawn(self.cmdline, timeout=2)
+        if DEBUG:
+            print self.description
+        else:
+            # note, trailing comma to suppress newline.
+            print self.description,
+
+        self.shell = pexpect.spawn(self.cmdline, timeout=TIMEOUT)
+
+        # suppress output when DEBUG is not set.
         if DEBUG:
             self.shell.logfile = sys.stdout
+
+        # generally don't want local echo, it gets confusing fast.
         self.shell.setecho(False)
         return self.shell
 
-    def __exit__(self, *dont_care):
+    def __exit__(self, t, v, tb):
+        global g_failures
         self.shell.close()
 
+        if not DEBUG:
+            # show result of test
+            if tb:
+                g_failures += 1
+                print "... Fail"
+            else:
+                print "... OK"
+            # Allow other tests to keep running
+            return True
+        else:
+            # Fail fast when in debug mode.
+            pass
 
-# Reproduce the bug as described in issue #1004
-with TestShell(SHELL) as osh:
+
+### Test Cases Below ##########################################################
+
+
+with InteractiveTest("Regression test for issue #1004") as osh:
     osh.expect(r'.*\$ ')
     osh.sendline("cat")
     stop_process__hack("cat")
@@ -56,8 +105,7 @@ with TestShell(SHELL) as osh:
     osh.expect("No job to put in the foreground")
 
 
-# Similar to above, but kill the process directly
-with TestShell(SHELL) as osh:
+with InteractiveTest("Test resuming a killed process") as osh:
     osh.expect(r'.*\$ ')
     osh.sendline("cat")
     stop_process__hack("cat")
@@ -65,15 +113,14 @@ with TestShell(SHELL) as osh:
     osh.expect(r".*\$")
     osh.sendline("fg")
     osh.expect(r"Continue PID \d+")
-    kill_process__hack("cat")
+    kill_process("cat")
     osh.expect(r"JobState NotifyDone \d+")
     osh.expect(r".*\$")
     osh.sendline("fg")
     osh.expect("No job to put in the foreground")
 
 
-# Reproduce the bug as described in 721
-with TestShell(SHELL) as osh:
+with InteractiveTest("Regression test for issue #721") as osh:
     osh.expect(r".*\$")
     osh.sendline("cat")
     osh.sendcontrol("c")
@@ -86,3 +133,8 @@ with TestShell(SHELL) as osh:
     osh.sendline("fg")
     osh.expect("No job to put in the foreground")
     osh.expect(r".*\$")
+
+
+### Don't touch this ##########################################################
+
+exit(g_failures)
