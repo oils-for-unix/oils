af01ea2487f41428ea1efbd676c4dc6ed87703b4
Andy C <andy@lenny>
2022-02-07 21:26:32 -0500

    [test/group-session] Fill out more of the test matrix
    - Discovered and inconsistency (?) in TPGID for bgproc and bgpipe cases.
      Not sure yet why it behaves this way.
    - Refactor to allow selecting multiple cases, e.g. fgpipe+bgpipe

--- test/group-session.sh
+++ test/group-session.sh
@@ -22,53 +22,79 @@ show_group_session() {
 
   local kind=$1
 
-  case $kind in
-    *fgpipe*)
-      echo '[foreground pipeline]'
-      ps -o pid,ppid,pgid,sid,tpgid,comm | cat | _tmp/cat2
-      ;;
+  # We have many case statements here to allow choosing many tests as a "mask",
+  # e.g. show_group_session fgproc+bgproc
 
-    *bgpipe*)
-      # For interactive shells, the TPGID is different here.
-      # Foreground: it matches the PGID of 'ps | cat | cat2'
-      # Background: it matches the PGID of bash!
+  case $kind in (*fgproc*)
+    echo '[foreground process]'
+    ps -o pid,ppid,pgid,sid,tgid,comm
+    ;;
+  esac
 
-      echo '[background pipeline]'
-      ps -o pid,ppid,pgid,sid,tpgid,comm | cat | _tmp/cat2 &
-      wait
-      ;;
+  case $kind in (*bgproc*)
+    echo '[background process]'
+    ps -o pid,ppid,pgid,sid,tgid,comm &
+    wait
 
-    *fgproc*)
-      echo '[single process]'
-      ps -o pid,ppid,pgid,sid,comm
-      ;;
+    # - Gets its own PGID
+    # - Hm UNLIKE bgpipe it also gets its own TGID?  Seems consistent in all
+    #   shells.  Why is that?
+    ;;
+  esac
 
-    *csub*)
-      # does NOT create a new process group.  So what happens when it's
-      # interrupted?
-      echo '[command sub]'
-      local x
-      x=$(ps -o pid,ppid,pgid,sid,comm)
-      echo "$x"
-      ;;
+  case $kind in (*fgpipe*)
+    echo '[foreground pipeline]'
+    ps -o pid,ppid,pgid,sid,tpgid,comm | cat | _tmp/cat2
+    ;;
+  esac
 
-    *psub*)
-      echo '[process sub]'
-      # use 'eval' as workaround for syntax error in dash and mksh
-      eval 'cat <(ps -o pid,ppid,pgid,sid,comm)'
-      # RESULTS
-      # zsh: ps and cat are in their own process groups distinct from the shell!
-      # bash: cat is in its own process group, but ps is in one with bash.  Hm
-      ;;
+  case $kind in (*bgpipe*)
+    # For interactive shells, the TPGID is different here.
+    # Foreground: it matches the PGID of 'ps | cat | cat2'
+    # Background: it matches the PGID of bash!
 
+    echo '[background pipeline]'
+    ps -o pid,ppid,pgid,sid,tpgid,comm | cat | _tmp/cat2 &
+    wait
+    ;;
+  esac
+
+  case $kind in (*subshell*)
+    # does NOT create a new process group.  So what happens when it's
+    # interrupted?
+
+    echo '[subshell]'
+    ( ps -o pid,ppid,pgid,sid,comm; echo ALIVE )
+    # subshell gets its own PGID in every shell!
+    ;;
+  esac
+
+  case $kind in (*csub*)
+    # does NOT create a new process group.  So what happens when it's
+    # interrupted?
+    echo '[command sub]'
+    local x
+    x=$(ps -o pid,ppid,pgid,sid,comm)
+    echo "$x"
+    ;;
+  esac
+
+  case $kind in (*psub*)
+    echo '[process sub]'
+    # use 'eval' as workaround for syntax error in dash and mksh
+    eval 'cat <(ps -o pid,ppid,pgid,sid,comm)'
+    # RESULTS
+    # zsh: ps and cat are in their own process groups distinct from the shell!
+    # bash: cat is in its own process group, but ps is in one with bash.  Hm
+    ;;
   esac
 }
 
 compare_shells() {
   ### Pass tasks, any of fgproc-fgpipe-bgpipe
 
-  #for sh in dash bash mksh zsh bin/osh; do
-  for sh in dash bash bin/osh; do
+  for sh in dash bash mksh zsh bin/osh; do
+  #for sh in dash bash bin/osh; do
 
   # for psub
   #for sh in bash zsh bin/osh; do
