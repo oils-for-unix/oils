2c807cf8164f6fda83340dbcf578b012f4619a0d
Andy C <andy@lenny>
2022-03-24 01:44:49 -0400

    [devtools] release-history script.
    - Iterate over releases
    - Extract HTML tables from test/spec.wwz files
    - Parse the total with a CSS selector

---
+++ devtools/release-history.sh
@@ -0,0 +1,163 @@
+#!/usr/bin/env bash
+#
+# Usage:
+#   devtools/release-history.sh <function name>
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+readonly ROOT=../oilshell.org__deploy
+readonly BASE_DIR=_tmp/release-history
+
+wwz-tsv() {
+  find $ROOT/release/ -mindepth 1 -maxdepth 1 -type d | sort | devtools/release_history.py
+
+  # TODO:
+  # - Have to sync the .wwz files for some releases.  I really want "vat" here.
+}
+
+print-row() {
+  local i=0
+  for cell in "$@"; do
+    if test $i -ne 0; then
+      echo -n $'\t'
+    fi
+    echo -n "$cell"
+    i=$((i + 1))
+  done
+  echo
+}
+
+extract-totals() {
+  local base_dir=$1
+
+  local -a header=()
+  local -a row=()
+
+  IFS=$'\t' read -a header
+  #argv "${header[@]}"
+
+  print-row release_date version survey_total cpp_total
+
+  local i=0
+  while IFS=$'\t' read -a row; do
+    local release_date=${row[0]}
+    local version=${row[1]}
+    local spec_wwz=${row[2]}
+    local survey_path=${row[3]}
+    local cpp_summary_path=${row[4]}
+
+    if test $spec_wwz = '-'; then
+      continue
+    fi
+    if test $survey_path = '-'; then
+      continue
+    fi
+    if test $cpp_summary_path = '-'; then
+      continue
+    fi
+
+    spec_wwz=$PWD/$spec_wwz
+
+    local d=$base_dir/$i
+    mkdir -p $d
+    pushd $d > /dev/null
+    # -o: overwrite without prompting
+    unzip -q -o $spec_wwz $survey_path
+    unzip -q -o $spec_wwz $cpp_summary_path
+    popd > /dev/null
+
+    local survey_total
+    survey_total=$(survey-total $d/$survey_path)  # strip trailing newline
+
+    local cpp_total
+    cpp_total=$(cpp-summary-total $d/$cpp_summary_path)
+
+    print-row "$release_date" $version $survey_total $cpp_total
+
+    #argv "${row[@]}"
+
+    i=$((i+1))
+  done
+}
+
+normalize() {
+  local path=$1
+
+  # hxselect requires a well-formed XML document
+  # NOTE: syntax error on "<tr class=>", but it correctly ignores it
+  hxnormalize -x $path > $path.xml || true
+}
+
+survey-total() {
+  ### Given a file, print the Python total to stdout
+
+  local path=$1
+
+  normalize $path
+  hxselect -s $'\n' -c '.totals:nth-child(1) td:nth-child(3)' < $path.xml | head -n 1
+}
+
+cpp-summary-total() {
+  ### Given a file, print the C++ total to stdout
+
+  local path=$1
+  normalize $path
+
+  # Change 1,137 -> 1137
+  < $path.xml hxselect -s $'\n' -c '#summary thead tr:nth-child(2) td:nth-child(5)' | sed 's/,//'
+}
+
+spec-tsv() {
+  rm -r -f -v $BASE_DIR
+  mkdir -p $BASE_DIR
+
+  wwz-tsv > $BASE_DIR/wwz.tsv
+
+  < $BASE_DIR/wwz.tsv extract-totals $BASE_DIR | tee $BASE_DIR/spec.tsv
+
+  wc -l $BASE_DIR/spec.tsv
+  echo
+  cat $BASE_DIR/spec.tsv | pretty-tsv
+
+  #find $BASE_DIR
+}
+
+
+deps-apt() { 
+  # https://superuser.com/questions/528709/command-line-css-selector-tool
+  sudo apt-get install html-xml-utils
+}
+
+# MEH these tools have bad error messages.
+demo() {
+  unzip -l $ROOT/release/0.9.8/test/spec.wwz || echo status=$?
+
+  # Annoying syntax for this command
+  unzip -p $ROOT/release/0.9.8/test/spec.wwz 'survey/osh.html'  > _tmp/osh.html
+  unzip -p $ROOT/release/0.9.8/test/spec.wwz 'cpp/osh-summary.html' > _tmp/osh-summary.html
+
+  local tmp=_tmp
+
+  # hxnormalize makes it XML.
+  # NOTE: syntax error on "<tr class=>", but it correctly ignores it
+  echo 'PYTHON'
+  survey-total _tmp/osh.html 
+
+  cpp-summary-total _tmp/osh-summary.html 
+  echo 'CPP'
+
+
+  return
+
+  # select the rows tagged class="totals", and then the 3rd <td> cell
+  cat _tmp/osh.xml | hxselect -s $'\n' -c '.totals td:nth-child(3)' | head -n 1
+  echo status=$?
+
+  # select the id="summary" table, then the header
+  cat _tmp/osh-summary.xml | hxselect -s $'\n' -c '#summary thead tr:nth-child(2) td:nth-child(5)' | sed 's/,//'
+  echo status=$?
+}
+
+"$@"
---
+++ devtools/release_history.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python2
+"""
+release_history.py
+"""
+from __future__ import print_function
+
+import os, re, subprocess, sys, zipfile
+
+def log(msg, *args):
+  if args:
+    msg = msg % args
+    print('\t' + msg, file=sys.stderr)
+
+VERSION_RE = re.compile('0\.(\d+)\.(\w+)')
+HEADER=('date', 'version', 'spec_wwz', 'survey_path', 'cpp_summary_path')
+
+def main(argv):
+
+  print('\t'.join(HEADER))
+
+
+  for release_dir in sys.stdin:
+    release_dir = release_dir.strip()
+
+    #print(release_dir)
+    m = VERSION_RE.search(release_dir)
+    assert m is not None
+
+    # For now, just do 0.8 and 0.9
+    version = m.group(0)
+    v2 = m.group(1)
+    v3 = m.group(2)
+    if v2 not in ('8', '9'):
+      continue
+
+    p = os.path.join(release_dir, 'release-date.txt')
+    with open(p) as f:
+      date = f.readline().strip()
+
+    # 0.9.3 is missing
+    #print('%s\t%s' % (date, version))
+    spec_wwz = os.path.join(release_dir, 'test/spec.wwz')
+    if not os.path.exists(spec_wwz):
+      log('No spec.wwz; skipping %s %s', date, version)
+      print('\t'.join([date, version, '-', '-', '-']))
+      continue
+
+    with open(spec_wwz) as f:
+      z = zipfile.ZipFile(f)
+
+      survey_path = 'survey/osh.html'
+      try:
+        z.getinfo(survey_path)
+      except KeyError:
+        survey_path = '-'
+
+      cpp_summary_path = 'cpp/osh-summary.html'
+      try:
+        h2 = z.getinfo(cpp_summary_path)
+      except KeyError:
+        cpp_summary_path = '-'
+
+    print('\t'.join([date, version, spec_wwz, survey_path, cpp_summary_path]))
+
+
+if __name__ == '__main__':
+  try:
+    main(sys.argv)
+  except RuntimeError as e:
+    print('FATAL: %s' % e, file=sys.stderr)
+    sys.exit(1)
