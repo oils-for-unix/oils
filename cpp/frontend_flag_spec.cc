7b72fc5956a5944be7e217c751aa9a73ce9e49c0
Andy C <andy@lenny>
2022-05-17 10:42:18 -0400

    [cpp] Fix crash because by missing return value!
    It is highly annoying that this is only a warning in C++!
    
    At runtime, I get a segfault that says "Illegal instruction".  AND ONLY
    ON SOME COMPILERS.
    
    How can we make -Wreturn-type a hard error?
    
    Also Clang has some bad warnings, which led me to ignore them.
    
        cpp/frontend_flag_spec.cc:299:1: warning: non-void function does not
        return a value [-Wreturn-type]
        }
        ^
    
    Also: unrelated doc change.

--- cpp/frontend_flag_spec.cc
+++ cpp/frontend_flag_spec.cc
@@ -295,7 +295,7 @@ Tuple2<args::_Attributes*, args::Reader*> ParseCmdVal(
     Str* spec_name, runtime_asdl::cmd_value__Argv* cmd_val,
     bool accept_typed_args) {
   // TODO: disallow typed args!
-  ParseCmdVal(spec_name, cmd_val);
+  return ParseCmdVal(spec_name, cmd_val);
 }
 
 Tuple2<args::_Attributes*, args::Reader*> ParseLikeEcho(
