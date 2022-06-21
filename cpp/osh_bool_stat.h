198335787bd4391c2645d9b65be9bee379eefe2d
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-06-21 11:02:57 -0700

    [cpp] Add test {-h, -L, -p, -O, -G, -u, -g}

--- cpp/osh_bool_stat.h
+++ cpp/osh_bool_stat.h
@@ -18,54 +18,81 @@ bool isatty(Str* fd_str, word_t* blame_word);
 inline bool DoUnaryOp(Id_t op_id, Str* s) {
   mylib::Str0 path(s);
 
-  // TODO: also call lstat(), etc.
-
   const char *zPath = path.Get();
 
-  struct stat st;
-  if (stat(zPath, &st) < 0) {
-    return false;
+  if (op_id == Id::BoolUnary_h ||
+      op_id == Id::BoolUnary_L )
+  {
+    struct stat st;
+    if (lstat(zPath, &st) < 0) {
+      return false;
+    }
+
+    return S_ISLNK(st.st_mode);
   }
+  else
+  {
+    struct stat st;
+    if (stat(zPath, &st) < 0) {
+      return false;
+    }
 
-  auto mode = st.st_mode;
+    auto mode = st.st_mode;
 
-  switch (op_id) {
-  // synonyms for existence
-  case Id::BoolUnary_a:
-  case Id::BoolUnary_e:
-    return true;
+    switch (op_id) {
+    // synonyms for existence
+    case Id::BoolUnary_a:
+    case Id::BoolUnary_e:
+      return true;
 
-  case Id::BoolUnary_s:
-    return st.st_size != 0;
+    case Id::BoolUnary_s:
+      return st.st_size != 0;
 
-  case Id::BoolUnary_d:
-    return S_ISDIR(mode);
+    case Id::BoolUnary_d:
+      return S_ISDIR(mode);
 
-  case Id::BoolUnary_f:
-    return S_ISREG(mode);
+    case Id::BoolUnary_f:
+      return S_ISREG(mode);
 
-  case Id::BoolUnary_k:
-    return (mode & S_ISVTX) != 0;
+    case Id::BoolUnary_k:
+      return (mode & S_ISVTX) != 0;
 
-  // NOTE(Jesse): The python OSH interpreter implements -r, -w, -x (in bool_stat.py)
-  // using posix.access(), which is effectively a thunk to C access().
-  //
-  // https://github.com/python/cpython/blob/8d999cbf4adea053be6dbb612b9844635c4dfb8e/Modules/posixmodule.c#L2547
-  //
-  // We cannot use the `stat` struct because the python analogue for these
-  // checks permission for the _calling_process_, which would end up being
-  // pretty obtuse if we used the information contained in the `stat` struct.
-  // It contains rwx information for owner, group, and other; we'd have
-  // to figure out which of those our process belonged to.
-    case Id::BoolUnary_x:
-      return access(zPath, X_OK) == 0;
-  //
-    case Id::BoolUnary_r:
-      return access(zPath, R_OK) == 0;
-  //
-    case Id::BoolUnary_w:
-      return access(zPath, W_OK) == 0;
-  // end note
+    case Id::BoolUnary_p:
+      return S_ISFIFO(mode);
+
+    case Id::BoolUnary_O:
+      return st.st_uid == geteuid();
+
+    case Id::BoolUnary_G:
+      return st.st_gid == getegid();
+
+    case Id::BoolUnary_u:
+      return mode & S_ISUID;
+
+    case Id::BoolUnary_g:
+      return mode & S_ISGID;
+
+
+    // NOTE(Jesse): The python OSH interpreter implements -r, -w, -x (in bool_stat.py)
+    // using posix.access(), which is effectively a thunk to C access().
+    //
+    // https://github.com/python/cpython/blob/8d999cbf4adea053be6dbb612b9844635c4dfb8e/Modules/posixmodule.c#L2547
+    //
+    // We cannot use the `stat` struct because the python analogue for these
+    // checks permission for the _calling_process_, which would end up being
+    // pretty obtuse if we used the information contained in the `stat` struct.
+    // It contains rwx information for owner, group, and other; we'd have
+    // to figure out which of those our process belonged to.
+      case Id::BoolUnary_x:
+        return access(zPath, X_OK) == 0;
+    //
+      case Id::BoolUnary_r:
+        return access(zPath, R_OK) == 0;
+    //
+      case Id::BoolUnary_w:
+        return access(zPath, W_OK) == 0;
+    // end note
+    }
   }
 
 
@@ -95,6 +122,8 @@ inline bool DoBinaryOp(Id_t op_id, Str* s1, Str* s2) {
     return m1 > m2;
   case Id::BoolBinary_ot:
     return m1 < m2;
+  case Id::BoolBinary_ef:
+    return st1.st_dev == st2.st_dev && st1.st_ino == st2.st_ino;
   }
 
   assert(0);
