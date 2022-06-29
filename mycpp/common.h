38bb9fcee2f99dc17d57e7309c4ec69d7a2ad943
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-06-29 12:52:29 -0700

    Change assert(0) -> InvalidCodePath() and add comments (#1180)

--- mycpp/common.h
+++ mycpp/common.h
@@ -8,6 +8,20 @@
 #include <cstdarg>  // va_list, etc.
 #include <cstdio>   // vprintf
 
+// TODO(Jesse): Put NotImplemented on a compile-time switch such that we cannot
+// make a release build if we're not finished implementing the interpreter.
+// ie.
+//
+// #if OIL_INTERNAL
+//   #define NotImplemented() assert(!"Not Implemented")
+// #else
+//   #define NotImplemented() NOT IMPLENTED !!! // Intentionally a compile error
+// #endif
+//
+//
+#define NotImplemented() assert(!"Not Implemented")
+#define InvalidCodePath() assert(!"Invalid Code Path")
+
 // Prevent silent copies
 
 #define DISALLOW_COPY_AND_ASSIGN(TypeName) \
