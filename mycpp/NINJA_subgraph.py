96107f13bc63abac3b8e095b8dda75595e1d496e
Andy C <andy@lenny>
2022-07-02 21:13:22 -0400

    [refactor] Rename to mylib_leaky_test.cc
    Following an earlier change.

--- mycpp/NINJA_subgraph.py
+++ mycpp/NINJA_subgraph.py
@@ -159,7 +159,7 @@ def ShouldSkipBenchmark(name):
 RUNTIME = ['mycpp/my_runtime.cc', 'mycpp/mylib2.cc', 'mycpp/gc_heap.cc']
 
 UNIT_TESTS = {
-    'mycpp/mylib_test': ['mycpp/mylib_leaky.cc'],
+    'mycpp/mylib_leaky_test': ['mycpp/mylib_leaky.cc'],
     'mycpp/gc_heap_test': ['mycpp/gc_heap.cc'],
     'mycpp/gc_stress_test': RUNTIME,
     'mycpp/my_runtime_test': RUNTIME,
