119e6c97886cf43f581501b6f490143f6190cdc7
Andy C <andy@lenny>
2022-07-09 20:27:27 -0400

    [soil] Store Clang tarball in ~/oil_DEPS in the container
    Because ~/oil is mounted at runtime, so ~/oil/_cache can't be seen.

--- soil/deps-binary.sh
+++ soil/deps-binary.sh
@@ -15,16 +15,28 @@ source build/common.sh
 
 readonly DEPS_DIR=$REPO_ROOT/../oil_DEPS
 
-# Used in build/common.sh
 download-clang() {
+
+  # download into $DEPS_DIR and not _cache because Dockerfile.clang stores the
+  # compressed version
+
   wget --no-clobber --directory _cache \
     https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/clang+llvm-14.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 }
 
 extract-clang() {
-  mkdir -p $DEPS_DIR
+  ### For developers
+
   pushd $DEPS_DIR
   time tar -x --xz < ../oil/_cache/clang+llvm-14.0.0*.tar.xz
+  popd
+}
+
+extract-clang-inside-container() {
+  ### For Dockerfile.clang
+
+  pushd $DEPS_DIR
+  time tar -x --xz < clang+llvm-14.0.0*.tar.xz
   popd
 }
 
