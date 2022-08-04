31e62a87a69a087efde93561c8c9449a57ef1f9f
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-08-04 13:43:40 -0700

    Fix build for asdl variants

--- asdl/test.sh
+++ asdl/test.sh
@@ -52,6 +52,7 @@ gen-cpp-test() {
     asdl/runtime.cc \
     mycpp/mylib_old.cc \
     mycpp/gc_heap.cc \
+    mycpp/leaky_types.cc \
     _build/cpp/hnode_asdl.cc \
     $tmp_dir/typed_arith_asdl.cc \
     $tmp_dir/typed_demo_asdl.cc 
@@ -91,6 +92,7 @@ gc-test() {
     mycpp/gc_heap.cc \
     mycpp/gc_builtins.cc \
     mycpp/gc_mylib.cc \
+    mycpp/leaky_types.cc \
     asdl/runtime.cc \
     _build/cpp/hnode_asdl.cc \
     $tmp_dir/demo_lib_asdl.cc \
