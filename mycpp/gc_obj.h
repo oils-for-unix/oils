be294bbf86927036f25272afff3d0717dd5412bf
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-08-06 06:57:44 -0700

    Move some gc code into headers

---
+++ mycpp/gc_obj.h
@@ -0,0 +1,61 @@
+#ifndef GC_OBJ_H
+#define GC_OBJ_H
+
+// Why do we need this macro instead of using inheritance?
+// - Because ASDL uses multiple inheritance for first class variants, but we
+//   don't want multiple IMPLEMENTATION inheritance.  Instead we just generate
+//   compatible layouts.
+// - Similarly, GlobalStr is layout-compatible with Str.  It can't inherit from
+//   Obj like Str, because of the constexpr issue with char[N].
+
+// heap_tag_: one of Tag::
+// type_tag_: ASDL tag (variant)
+// field_mask_: for fixed length records, so max 16 fields
+// obj_len_: number of bytes to copy
+//   TODO: with a limitation of ~15 fields, we can encode obj_len_ in
+//   field_mask_, and save space on many ASDL types.
+//   And we can sort integers BEFORE pointers.
+
+// TODO: ./configure could detect big or little endian, and then flip the
+// fields in OBJ_HEADER?
+//
+// https://stackoverflow.com/questions/2100331/c-macro-definition-to-determine-big-endian-or-little-endian-machine
+//
+// Because we want to do (obj->heap_tag_ & 1 == 0) to distinguish it from
+// vtable pointer.  We assume low bits of a pointer are 0 but not high bits.
+
+#define OBJ_HEADER()    \
+  uint8_t heap_tag_;    \
+  uint8_t type_tag_;    \
+  uint16_t field_mask_; \
+  uint32_t obj_len_;
+
+class Obj {
+  // The unit of garbage collection.  It has a header describing how to find
+  // the pointers within it.
+  //
+  // Note: Sorting ASDL fields by (non-pointer, pointer) is a good idea, but it
+  // breaks down because mycpp has inheritance.  Could do this later.
+
+ public:
+  // Note: ASDL types are layout-compatible with Obj, but don't actually
+  // inherit from it because of the 'multiple inheritance of implementation'
+  // issue.  So they don't call this constructor.
+  constexpr Obj(uint8_t heap_tag, uint16_t field_mask, int obj_len)
+      : heap_tag_(heap_tag),
+        type_tag_(0),
+        field_mask_(field_mask),
+        obj_len_(obj_len) {
+  }
+
+  void SetObjLen(int obj_len) {
+    this->obj_len_ = obj_len;
+  }
+
+  OBJ_HEADER()
+
+  DISALLOW_COPY_AND_ASSIGN(Obj)
+};
+
+
+#endif  // GC_OBJ_H
--- mycpp/gc_types.h
+++ mycpp/gc_types.h
@@ -9,51 +9,7 @@
 
 namespace gc_heap {
 
-template <typename T>
-inline void InitSlabCell(Obj* obj) {
-  // log("SCANNED");
-  obj->heap_tag_ = Tag::Scanned;
-}
-
-template <>
-inline void InitSlabCell<int>(Obj* obj) {
-  // log("OPAQUE");
-  obj->heap_tag_ = Tag::Opaque;
-}
-
-// don't include items_[1]
-const int kSlabHeaderSize = sizeof(Obj);
-
-// Opaque slab, e.g. for List<int>
-template <typename T>
-class Slab : public Obj {
- public:
-  Slab(int obj_len) : Obj(0, 0, obj_len) {
-    InitSlabCell<T>(this);
-  }
-  T items_[1];  // variable length
-};
-
-template <typename T, int N>
-class GlobalSlab {
-  // A template type with the same layout as Str with length N-1 (which needs a
-  // buffer of size N).  For initializing global constant instances.
- public:
-  OBJ_HEADER()
-
-  T items_[N];
-
-  DISALLOW_COPY_AND_ASSIGN(GlobalSlab)
-};
-
-// Note: entries will be zero'd because the Heap is zero'd.
-template <typename T>
-inline Slab<T>* NewSlab(int len) {
-  int obj_len = RoundUp(kSlabHeaderSize + len * sizeof(T));
-  void* place = gHeap.Allocate(obj_len);
-  auto slab = new (place) Slab<T>(obj_len);  // placement new
-  return slab;
-}
+#include "mycpp/gc_slab.h"
 
 #ifdef MYLIB_LEAKY
   #define GLOBAL_STR(name, val) Str* name = StrFromC(val);
@@ -745,7 +701,6 @@ void Dict<K, V>::set(K key, V val) {
 // Do some extra calculation to avoid storing redundant lengths.
 inline int len(const gc_heap::Str* s) {
   assert(s->obj_len_ > gc_heap::kStrHeaderSize - 1);
-
   return s->obj_len_ - gc_heap::kStrHeaderSize - 1;
 }
 
