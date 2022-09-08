22458058e46a6ef2b11557ccf1ad06b98aa2cba8
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-09-08 16:19:17 -0600

    Tweaks to unity.sh

--- unity.sh
+++ unity.sh
@@ -1,6 +1,23 @@
+#!/usr/bin/env bash
 
-clang++                 \
-  -I .                  \
-  -Wno-invalid-offsetof \
-  all.cc
 
+echo "DOING FULL CLEAN"
+
+build/clean.sh > /dev/null 2>&1
+build/py.sh all > /dev/null 2>&1
+./NINJA-config.sh > /dev/null 2>&1
+
+echo "SETTING UP FOR FULL OSH_EVAL REBUILD"
+
+build/dev.sh all > /dev/null 2>&1
+
+echo "TIMING FULL REBUILD"
+
+time build/dev.sh oil-cpp > /dev/null 2>&1
+
+echo ""
+echo "TIMING UNITY BUILD"
+time clang++                \
+      -I .                  \
+      -Wno-invalid-offsetof \
+      all.cc > /dev/null 2>&1
