d2ad79a73c11785c90a3b1c3e652a04ca8a974bd
Melvin Walls <mwalls67@gmail.com>
2022-11-11 12:16:15 -0800

    [cpp] use RootingScope in QSN bindings

--- cpp/qsn.h
+++ cpp/qsn.h
@@ -31,10 +31,12 @@ inline bool IsPlainChar(Str* ch) {
 }
 
 inline Str* XEscape(Str* ch) {
+  RootingScope _r;
   assert(len(ch) == 1);
   StackRoots _roots({&ch});
   Str* result = NewStr(4);
   sprintf(result->data(), "\\x%02x", ch->data_[0] & 0xff);
+  gHeap.RootOnReturn(result);
   return result;
 }
 
@@ -43,9 +45,11 @@ inline Str* UEscape(int codepoint) {
   // 3 for \u{
   // 6 for codepoint
   // 1 for }
+  RootingScope _r;
   Str* result = OverAllocatedStr(10);
   int n = sprintf(result->data(), "\\u{%x}", codepoint);
   result->SetObjLenFromStrLen(n);  // truncate to what we wrote
+  gHeap.RootOnReturn(result);
   return result;
 }
 
