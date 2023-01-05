449cc58b52abad5fc94d20cb6c70f90a6bf7f307
Andy C <andy@lenny>
2023-01-05 00:45:23 -0500

    [demo] Test Ninja and Make

--- demo/matchertext.sh
+++ demo/matchertext.sh
@@ -23,14 +23,23 @@ m-extensible() {
   echo
 }
 
+# Other languages to test:
+#
+# - CSV -- seems unlikely that there is any syntactic room
+# - YAML -- maybe in its JSON subset, however most people seem to use the
+#   indented strings with a whitespace rule
+# - HTML and XML - addressed as '*ML' in the paper
+
 demo() {
-  echo 'Are the C-style string literals in this language M-extensible?'
-  echo 'We simply test them for syntax errors after a \'
+  echo 'Are the string literals in this language M-extensible?'
+  echo 'We simply test them for syntax errors after a special char like \'
   echo
   echo 'This is also relevant to YSTR, where we add \xff and \u{012345} escapes'
   echo
 
-  local tmp=/tmp/foo.c
+  mkdir -p _tmp
+
+  local tmp=_tmp/foo.c
 
   cat >$tmp <<'EOF'
 import json
@@ -38,6 +47,39 @@ json.loads('"\[]"')
 json.loads('"\m[]"')
 EOF
   m-extensible 'JSON' python3 $tmp
+
+
+  # The only metacharacter in Ninja is $, and a literal dollar is $$ (similar
+  # to GNU make)
+  #
+  # You could imagine a matchertext literal as $[ cp $SHELL_VAR_NOT_NINJA_VAR x ]
+  #
+  # Ninja and GNU make's conflict with shell annoys me
+
+  echo foo > _tmp/ninja-in
+
+  cat >_tmp/z.ninja <<'EOF'
+rule copy
+  command = cp $in $out
+
+build _tmp/out : copy _tmp/ninja-in
+
+build _tmp/$[ : copy _tmp/ninja-in
+
+EOF
+  m-extensible 'Ninja' ninja -f _tmp/z.ninja
+
+
+  echo foo > _tmp/make-in
+
+  cat >_tmp/z.mk <<'EOF'
+_tmp/make-out : _tmp/make-in
+	cp $< $@
+
+_tmp/make-out : _tmp/make-in
+	cp $[ $< $@
+EOF
+  m-extensible 'GNU Make' make -f _tmp/z.mk
 
   cat >$tmp <<'EOF'
 #include <stdio.h>
@@ -47,7 +89,6 @@ int main() {
 }
 EOF
   m-extensible 'C' gcc -o /tmp/m $tmp
-
 
   echo 'Running C'
 
@@ -80,7 +121,6 @@ echo "\m[]"
 console.log("\[]");
 console.log("\m[]");
 '
-
 }
 
 "$@"
