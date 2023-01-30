9f7572405d047517802f601fe28185fe39d58ebf
Andy C <andy@lenny>
2023-01-30 18:11:01 -0500

    [mycpp/examples] New test for global variables
    There is an oddity with HeapTag::Scanned instead of HeapTag::Global, but
    it doesn't cause a visible problem.
    
    The mark set will be to big, and children will be traced.

---
+++ mycpp/examples/test_globals.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python2
+"""
+test_globals.py
+"""
+from __future__ import print_function
+
+import os
+
+from mycpp import mylib
+from mycpp.mylib import log
+
+from typing import List
+
+
+class MyClass(object):
+
+  def __init__(self, x):
+    # type: (int) -> None
+    self.x = x
+
+  def Print(self):
+    # type: () -> None
+    log("x = %d", self.x)
+
+
+# Note: the C++ code for this object uses GC_CLASS_SCANNED / HeapTag::Scanned,
+# but HeapTag::Global would make more sense.
+#
+# So it gets put in the mark set, but it doesn't cause a crash because it's
+# never in the live_objs_ list, and thus not considered for sweeping.
+
+gFoo = MyClass(5)
+
+
+def f():
+  # type: () -> None
+  gFoo.Print()
+
+
+def run_tests():
+  # type: () -> None
+
+  for i in xrange(10):
+    f()
+    mylib.MaybeCollect()
+
+    new_obj = MyClass(42)
+    new_obj.Print()
+
+
+def run_benchmarks():
+  # type: () -> None
+  raise NotImplementedError()
+
+
+if __name__ == '__main__':
+  if os.getenv('BENCHMARK'):
+    log('Benchmarking...')
+    run_benchmarks()
+  else:
+    run_tests()
