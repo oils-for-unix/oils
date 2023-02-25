938b21d30e3a2e2d68d651e04bac71f287a51159
Andy C <andy@lenny>
2023-02-24 23:06:14 -0500

    [configure] Restore --without-readline
    And refactor to use early return style.
    
    Aided by nice new tests!

--- configure-test.sh
+++ configure-test.sh
@@ -10,7 +10,7 @@
 
 export _OIL_CONFIGURE_TEST=1  # so we don't run main
 
-. $PWD/configure  # define the functions to be test
+. $PWD/configure  # define the functions to be tested
 
 test_cc_statements() {
   cc_print_expr 'sizeof(int)'
@@ -103,53 +103,53 @@ test_echo_vars() {
   local output
 
   # before calling detect_readline
-  output="$(echo_vars 2>&1)"
+  output="$(echo_shell_vars 2>&1)"
   if test "$?" = 0; then
-    die 'Expected echo_vars to fail, but succeeded'
+    die 'Expected echo_shell_vars to fail, but succeeded'
   fi
-  if ! test "$output" = "$0 ERROR: called echo_vars before detecting readline."; then
-    die "Unexpected echo_vars output: $output"
+  if ! test "$output" = "$0 ERROR: called echo_shell_vars before detecting readline."; then
+    die "Unexpected echo_shell_vars output: $output"
   fi
 
-  # pretend detected_readline was called
+  # pretend detect_readline was called
   detected_readline=1
 
   # no readline
-  output="$(echo_vars)"
+  output="$(echo_shell_vars)"
   if ! test "$?" = 0; then
-    die 'Expected echo_vars to succeed, but failed'
+    die 'Expected echo_shell_vars to succeed, but failed'
   fi
   if ! test "$output" = 'HAVE_READLINE=
 PREFIX=/usr/local
 DATAROOTDIR='; then
-    die "Unexpected echo_vars output: $output"
+    die "Unexpected echo_shell_vars output: $output"
   fi
 
   # have readline, no readline_dir
   have_readline=1
-  output="$(echo_vars)"
+  output="$(echo_shell_vars)"
   if ! test "$?" = 0; then
-    die 'Expected echo_vars to succeed, but failed'
+    die 'Expected echo_shell_vars to succeed, but failed'
   fi
   if ! test "$output" = 'HAVE_READLINE=1
 READLINE_DIR=
 PREFIX=/usr/local
 DATAROOTDIR='; then
-    die "Unexpected echo_vars output: $output"
+    die "Unexpected echo_shell_vars output: $output"
   fi
 
    # have readline, readline_dir present
   have_readline=1
   readline_dir=/path/to/readline
-  output="$(echo_vars)"
+  output="$(echo_shell_vars)"
   if ! test "$?" = 0; then
-    die 'Expected echo_vars to succeed, but failed'
+    die 'Expected echo_shell_vars to succeed, but failed'
   fi
   if ! test "$output" = 'HAVE_READLINE=1
 READLINE_DIR=/path/to/readline
 PREFIX=/usr/local
 DATAROOTDIR='; then
-    die "Unexpected echo_vars output: $output"
+    die "Unexpected echo_shell_vars output: $output"
   fi
 
   # clean-up
@@ -159,10 +159,19 @@ DATAROOTDIR='; then
 }
 
 soil_run() {
-  test_cc_statements
-  test_parse_flags
-  test_echo_cpp
-  test_echo_vars
+  # Note: could use run-test-funcs
+
+  for func in \
+    test_cc_statements \
+    test_parse_flags \
+    test_echo_cpp \
+    test_echo_vars; do
+
+    echo "    $func"
+    $func
+    echo '    OK'
+
+  done
 }
 
 "$@"
