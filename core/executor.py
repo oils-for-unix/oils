96eb0ab423dd32a20135bfa5b4ef09c933d0241e
Andy C <andy@lenny>
2023-03-13 23:13:45 -0400

    [core refactor] Separate code paths for regular and & pipelines
    They're even more different with job control.  Background pipelines
    don't get a PGID because they don't get terminal signals.

--- core/executor.py
+++ core/executor.py
@@ -286,7 +286,7 @@ class ShellExecutor(vm._Executor):
         p.Init_ParentPipeline(pi)
         pi.Add(p)
 
-      pi.StartPipeline(self.waiter)
+      pi.StartInBackground()
       last_pid = pi.LastPid()
       self.mem.last_bg_pid = last_pid   # for $!
 
--- core/process.py
+++ core/process.py
@@ -1077,12 +1077,10 @@ class Pipeline(Job):
 
     self.last_pipe = (r, w)  # So we can connect it to last_thunk
 
-  def StartPipeline(self, waiter):
-    # type: (Waiter) -> None
-    # TODO: pipelines should be put in their own process group with setpgid().
-    # I tried 'cat | cat' and Ctrl-C, and it works without this, probably
-    # because of SIGPIPE?  I think you will need that for Ctrl-Z, to suspend a
-    # whole pipeline.
+  def StartInBackground(self):
+    # type: () -> None
+    """ For background pipelines e.g. sleep 5 | ls & """
+    assert self.last_thunk is None
 
     for i, proc in enumerate(self.procs):
       pid = proc.StartProcess(trace.PipelinePart())
@@ -1094,8 +1092,22 @@ class Pipeline(Job):
       # from non-adjacent pipes.
       proc.MaybeClosePipe()
 
-    if self.last_thunk:
-      self.pipe_status.append(-1)  # for self.last_thunk
+  def _Start(self):
+    # type: () -> None
+    """ For sleep 5 | ls, which may be a foreground pipeline """
+
+    for i, proc in enumerate(self.procs):
+      pid = proc.StartProcess(trace.PipelinePart())
+      self.pids.append(pid)
+      self.pipe_status.append(-1)  # uninitialized
+
+      # NOTE: This is done in the SHELL PROCESS after every fork() call.
+      # It can't be done at the end; otherwise processes will have descriptors
+      # from non-adjacent pipes.
+      proc.MaybeClosePipe()
+
+    assert self.last_thunk is not None
+    self.pipe_status.append(-1)  # for self.last_thunk
 
   def LastPid(self):
     # type: () -> int
@@ -1144,7 +1156,7 @@ class Pipeline(Job):
     """
     assert len(self.procs) != 0
 
-    self.StartPipeline(waiter)
+    self._Start()
 
     # Run the last part of the pipeline IN PARALLEL with other processes.  It
     # may or may not fork:
