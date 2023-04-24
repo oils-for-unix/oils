779eb9b9f00d90c78faebee2de7b29636fe52dca
andychu <andychu@users.noreply.github.com>
2023-04-24 11:10:14 -0400

    Fix help and indent

--- devtools/run-task.sh
+++ devtools/run-task.sh
@@ -3,29 +3,29 @@
 # Common shell functions for task scripts.
 #
 # Usage:
-#   source devtools/common.sh
+#   source devtools/run-task.sh
 #    ...  # define task functions
 #   run-task "$@"
 
 # List all functions defined in this file (and not in sourced files).
 _list-funcs() {
-    local funcs=($(compgen -A function))
-    # extdebug makes `declare -F` print the file path, but, annoyingly, only
-    # if you pass the function names as arguments.
-    shopt -s extdebug
-    declare -F "${funcs[@]}" | grep --fixed-strings " $0" | awk '{print $1}'
+  local funcs=($(compgen -A function))
+  # extdebug makes `declare -F` print the file path, but, annoyingly, only
+  # if you pass the function names as arguments.
+  shopt -s extdebug
+  declare -F "${funcs[@]}" | grep --fixed-strings " $0" | awk '{print $1}'
 }
 
 run-task() {
   if [[ $# -eq 0 || $1 =~ ^(--help|-h)$ ]]; then
-      echo "Usage: $0 <function> [<args>]"
-      echo
-      echo "For bash completion of available functions, run:"
-      echo "   source devtools/completion.bash"
-      echo
-      echo "Available functions:"
-      _list-funcs | column
-      exit
+    echo "Usage: $0 <function> [<args>]"
+    echo
+    echo "For bash completion of available functions, run:"
+    echo "   source devtools/completion.bash"
+    echo
+    echo "Available functions:"
+    _list-funcs | column
+    exit
   fi
   "$@"
 }
