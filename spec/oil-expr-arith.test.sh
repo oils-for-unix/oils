8b1047257508b5adf8af09a4a592bc3a50378242
Melvin Walls <mwalls67@gmail.com>
2023-04-26 22:42:23 -0700

    [oil_lang] more refactor prep, refactor unary expression to value_t (#1588)
    * [oil_lang] split more types out of _EvalExpr()
    
    This has no functional impact. It will just help with readability while
    we do some refactoring here.
    
    * [oil_lang] refactor unary expression to value_t
    
    * narrow acceptable types for unary operands
    
    * add spec tests for unary ops

--- spec/oil-expr-arith.test.sh
+++ spec/oil-expr-arith.test.sh
@@ -46,3 +46,52 @@ write -- $m $n $p
 1
 25
 ## END
+
+#### Unary Operations
+
+var a = ~1
+
+var b = -1
+var c = -2.3
+
+var d = not true
+
+
+write -- $a $b $c $d
+
+## STDOUT:
+-2
+-1
+-2.3
+false
+## END
+
+#### unary ~ applied to bool is not allowed
+= ~false
+## status: 3
+## STDOUT:
+## END
+
+#### unary ~ applied to float is not allowed
+= ~1.
+## status: 3
+## STDOUT:
+## END
+
+#### unary - applied to bool is not allowed
+= ~false
+## status: 3
+## STDOUT:
+## END
+
+#### unary 'not' applied to int is not allowed
+= not 1
+## status: 3
+## STDOUT:
+## END
+
+#### unary 'not' applied to float is not allowed
+= not 1.
+## status: 3
+## STDOUT:
+## END
