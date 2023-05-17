ca4d245034de678b339cbcd54dc62bd3481998e6
Andy C <andy@lenny>
2023-05-16 21:09:15 -0400

    [frontend cleanup] Remove unused code

--- devtools/refactor.sh
+++ devtools/refactor.sh
@@ -201,14 +201,7 @@ spid-sig() {
     egrep -n 'def.*(span_id|spid)' */*.py
 }
 
-# 2023-05: 21 instances
-get-token() {
-  # Memory leak
-  show-usages _tmp/get-token \
-    egrep -n 'GetToken' */*.py
-}
-
-# 2023-05: 23 instances
+# 2023-05: 13 instances
 of-word() {
   show-usages _tmp/of-word \
     egrep -n 'OfWord' */*.py
