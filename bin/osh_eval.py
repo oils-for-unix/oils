44ff2e8a9fe870752c57efa8b2b4cb85979cdb5b
Andy Chu <andy@oilshell.org>
2023-05-21 15:34:46 -0400

    [test/lint] Fix build

--- bin/osh_eval.py
+++ bin/osh_eval.py
@@ -7,7 +7,6 @@ from __future__ import print_function
 import sys
 
 from _devbuild.gen.syntax_asdl import CompoundWord
-from asdl import runtime
 from core import error
 from core import shell
 from core import pyos
