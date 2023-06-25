7b9b5d47a7a24d1bcd12a90e0010642c425ef4bc
Andy C <andy@lenny>
2023-06-25 10:58:12 -0400

    [ysh] Merge part of PR to statically type builtin functions
    Originally PR #1644
    
    No change in behavior yet.
    
    Co-authored-by: Melvin Walls <mwalls67@gmail.com>

--- core/util.py
+++ core/util.py
@@ -10,7 +10,12 @@ util.py - Common infrastructure.
 """
 from __future__ import print_function
 
+from _devbuild.gen.runtime_asdl import value, value_e, value_str, value_t
+from _devbuild.gen.syntax_asdl import loc
+from core import error
 from mycpp import mylib
+
+from typing import cast
 
 
 class UserExit(Exception):
@@ -75,3 +80,47 @@ class DebugFile(_DebugFile):
         # type: () -> bool
         """Used by node.PrettyPrint()."""
         return self.f.isatty()
+
+
+def MustBeInt(val):
+    # type: (value_t) -> value.Int
+    UP_val = val
+    if val.tag() == value_e.Int:
+        val = cast(value.Int, UP_val)
+        return val
+
+    raise error.InvalidType(
+        'expected value.Int, but got %s' % value_str(val.tag()), loc.Missing)
+
+
+def MustBeStr(val):
+    # type: (value_t) -> value.Str
+    UP_val = val
+    if val.tag() == value_e.Str:
+        val = cast(value.Str, UP_val)
+        return val
+
+    raise error.InvalidType(
+        'expected value.Str, but got %s' % value_str(val.tag()), loc.Missing)
+
+
+def MustBeList(val):
+    # type: (value_t) -> value.List
+    UP_val = val
+    if val.tag() == value_e.List:
+        val = cast(value.List, UP_val)
+        return val
+
+    raise error.InvalidType(
+        'expected value.List, but got %s' % value_str(val.tag()), loc.Missing)
+
+
+def MustBeFunc(val):
+    # type: (value_t) -> value.Func
+    UP_val = val
+    if val.tag() == value_e.Func:
+        val = cast(value.Func, UP_val)
+        return val
+
+    raise error.InvalidType(
+        'expected value.Func, but got %s' % value_str(val.tag()), loc.Missing)
