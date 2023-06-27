9b8ab95edf7e63e4773c7c704bda47828ce85672
Andy C <andy@lenny>
2023-06-27 00:17:31 -0400

    [fix] Typo bug in last commit

--- core/error.py
+++ core/error.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 
 from _devbuild.gen.syntax_asdl import loc_e, loc
-from _devbuild.gen.runtime_asdl import value, value_t, value_str
+from _devbuild.gen.runtime_asdl import value_t, value_str
 
 from typing import TYPE_CHECKING, NoReturn
 if TYPE_CHECKING:
