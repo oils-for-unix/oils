a898df8dc3cfe629d3586ab5089e3f02934f9d8a
Aidan Olsen <a.olsen2024@icloud.com>
2023-07-14 21:14:37 -0600

    Add more tests over func call evaluation

--- spec/ysh-funcs.test.sh
+++ spec/ysh-funcs.test.sh
@@ -12,3 +12,27 @@ func identity(x) {
 ## STDOUT:
 (Str)   'ysh'
 ## END
+
+#### Too many args
+func f(x) { return (x + 1) }
+
+= f(0, 1)
+## status: 3
+## STDOUT:
+## END
+
+#### Too few args
+func f(x) { return (x + 1) }
+
+= f()
+## status: 3
+## STDOUT:
+## END
+
+#### Proc-style return in a func
+func t() { return 0 }
+
+= t()
+## status: 1
+## STDOUT:
+## END
