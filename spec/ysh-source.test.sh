c8518b8e9135142cac756733187cf312ed339caf
Aidan Olsen <a.olsen2024@icloud.com>
2023-08-16 22:49:20 -0600

    Explicitly test stdlib/math.ysh with shopt --set ysh:upgrade

--- spec/ysh-source.test.sh
+++ spec/ysh-source.test.sh
@@ -1,10 +1,11 @@
 # spec/ysh-source
 
-## our_shell: ysh
 ## oils_failures_allowed: 0
 
 #### --builtin flag
-source --builtin stdlib/math.ysh
+shopt --set ysh:upgrade
+
+source --builtin math.ysh
 
 json write (max(1, 2))
 ## STDOUT:
@@ -12,12 +13,16 @@ json write (max(1, 2))
 ## END
 
 #### no path passed with --builtin flag
+shopt --set ysh:upgrade
+
 source --builtin
 ## status: 2
 ## STDOUT:
 ## END
 
 #### non-existant path passed to --builtin flag
+shopt --set ysh:upgrade
+
 source --builtin test/this-file-will-never-exist.ysh
 ## status: 2
 ## STDOUT:
