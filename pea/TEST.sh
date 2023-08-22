f2367ead53f7cd82c2d5745d16eadf4365190dfa
Andy C <andy@oilshell.org>
2023-08-22 14:48:04 -0400

    [pea] Nice test of parallel parsing
    We can parse in under 300ms by using 8 cores.
    
    It would nice to parse, type check, and emit C++ for the whole codebase
    in under a second.
    
    Or just parsing and type checking in one second would be an improvement.
    
    I think that should be easily doable, maybe even in Python.  Maybe we
    can use fast 'marshal' serialization or something.
    
    MyPy (without mycpp speedups) is taking 4-5 seconds to parse and type
    check.
    
    With the mycpp speedups, it's possible it will get down to 1 second, not
    sure.  Could investigate that build.

--- pea/TEST.sh
+++ pea/TEST.sh
@@ -41,6 +41,85 @@ parse-all() {
   time all-files | xargs --verbose -- $0 parse-one
 }
 
+# Good illustration of "distributing your overhead"
+#
+# Total work goes up, while latency goes down.  To a point.  Then it goes back
+# up.
+
+# batch size 30
+# 
+# real    0m0.342s
+# user    0m0.735s
+# sys     0m0.059s
+# 
+# batch size 20
+# 
+# real    0m0.305s
+# user    0m0.993s
+# sys     0m0.081s
+# 
+# batch size 15
+# 
+# real    0m0.299s
+# user    0m1.110s
+# sys     0m0.123s
+# 
+# batch size 10
+# 
+# real    0m0.272s
+# user    0m1.362s
+# sys     0m0.145s
+
+parallel-test() {
+  ### Test out parallelism of Python processes
+
+  local files
+  num_files=$(all-files | wc -l)
+
+  # 103 files
+
+  # try different batch sizes.  
+  local p=$(( $(nproc) ))
+
+  echo "Parsing $num_files files with $p parallel processes"
+  echo
+
+  # 12 files at a time does well compared to the fastest
+  local optimal=$(( num_files / (p - 1) ))
+  echo "Optimal batch size is $optimal"
+  echo
+
+  echo 'All at once:'
+  time parse-all > /dev/null 2>&1
+  echo
+
+  # 5 is meant to be very suboptimal
+  for n in 50 30 20 10 5 $optimal; do
+    echo "batch size $n"
+    time all-files | xargs --verbose -P $p -n $n -- $0 parse-one > /dev/null 2>&1
+    echo
+  done
+}
+
+# MyPy dev version takes 10.2 seconds the first time (without their mypyc
+# speedups)
+#
+# 0.150 seconds the second time, WITHOUT code changes
+# 0.136 seconds
+
+# 4.1 seconds: whitespace change
+# 3.9 seconds: again, and this is on my fast hoover machine
+
+# 5.0 seconds - Invalid type!
+# 4.9 seconds - again invalid
+
+
+mypy-compare() {
+  devtools/types.sh check-oils
+}
+
+
+
 # TODO: Fix INTERNAL ERROR with MyPy 0.782
 # We're using that for Python 2 support, but this is Python 3
 check-types() {
