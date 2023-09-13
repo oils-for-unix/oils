8e88cc55035df4d99e360ab6b4964ff0266b791c
Andy C <andy@lenny>
2023-09-12 22:32:52 -0400

    [release] Fix typo

--- devtools/release.sh
+++ devtools/release.sh
@@ -391,7 +391,7 @@ _compressed-tarball() {
   local out=_release/$name-$version.tar.gz
 
   # Overwrite it to cause rebuild of oil.tar
-  build/ovm-actions.sh write-release-date
+  build/stamp.sh write-release-date
 
   #make -d -r $in  # To debug
   make $in
