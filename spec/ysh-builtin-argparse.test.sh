91f416bb9046c2f6e72cee1dac53885a6460eca0
Aidan Olsen <a.olsen2024@icloud.com>
2023-09-21 09:36:51 -0600

    [stdlib/args] Implement parseArgs, add tests using an explicit JSON spec

--- spec/ysh-builtin-argparse.test.sh
+++ spec/ysh-builtin-argparse.test.sh
@@ -72,3 +72,73 @@ options:
  -h, --help           show this help message and exit
  -v, --verbose        Verbose
 ## END
+
+#### Parse args using a JSON argspec
+source --builtin args.ysh
+
+var spec = {
+  flags: [
+    {short: '-v', long: '--verbose', type: null, default: '', help: 'Enable verbose logging'},
+    {short: '-c', long: '--count', type: 'int', default: 80, help: 'Maximum line length'},
+  ],
+  args: [
+    {name: 'file', type: 'str', help: 'File to check line lengths of'}
+  ],
+  rest: false,
+}
+
+var argsCases = [
+  :| -v --count 120 example.sh |,
+  :| -v --count 120 example.sh -v |,  # duplicate flags are ignored
+  :| -v --count 120 example.sh -v --count 150 |,  # the last duplicate has precedence
+]
+
+var argparse_py = '''
+import argparse
+import sys
+
+spec = argparse.ArgumentParser()
+spec.add_argument("filename")
+spec.add_argument("-c", "--count")
+spec.add_argument("-v", "--verbose",
+                  action="store_true")
+
+result = spec.parse_args(sys.argv[1:])
+print(result)
+'''
+
+for args in (argsCases) {
+  var args_str = args->join(" ")
+  echo "----------  $args_str  ----------"
+  echo "\$ bin/ysh example.sh $args_str"
+  = parseArgs(spec, args)
+
+  echo
+  echo "\$ python3 example.py $args_str"
+  python3 -c $argparse_py @args
+
+  echo
+}
+## STDOUT:
+----------  -v --count 120 example.sh  ----------
+$ bin/ysh example.sh -v --count 120 example.sh
+(List)   [<'verbose': True, 'count': 120, 'file': 'example.sh'>, 4]
+
+$ python3 example.py -v --count 120 example.sh
+Namespace(filename='example.sh', count='120', verbose=True)
+
+----------  -v --count 120 example.sh -v  ----------
+$ bin/ysh example.sh -v --count 120 example.sh -v
+(List)   [<'verbose': True, 'count': 120, 'file': 'example.sh'>, 5]
+
+$ python3 example.py -v --count 120 example.sh -v
+Namespace(filename='example.sh', count='120', verbose=True)
+
+----------  -v --count 120 example.sh -v --count 150  ----------
+$ bin/ysh example.sh -v --count 120 example.sh -v --count 150
+(List)   [<'verbose': True, 'count': 150, 'file': 'example.sh'>, 7]
+
+$ python3 example.py -v --count 120 example.sh -v --count 150
+Namespace(filename='example.sh', count='150', verbose=True)
+
+## END
