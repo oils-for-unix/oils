2c59733e5489806da70d5c03a49ec5731c419264
Aidan <46799759+PossiblyAShrub@users.noreply.github.com>
2023-10-17 22:42:17 -0600

    [stdlib] First cut of declarative flag parsing, in args.ysh (#1736)

--- test/spec.sh
+++ test/spec.sh
@@ -717,6 +717,10 @@ ysh-bin() {
   run-file ysh-bin "$@"
 }
 
+ysh-argparse() {
+  run-file ysh-argparse "$@"
+}
+
 ysh-array() {
   run-file ysh-array "$@"
 }
@@ -737,21 +741,16 @@ ysh-builtins() {
   run-file ysh-builtins "$@"
 }
 
-ysh-builtin-eval() {
-  run-file ysh-builtin-eval "$@"
-}
-
-
 ysh-builtin-module() {
   run-file ysh-builtin-module "$@"
 }
 
-ysh-builtin-argparse() {
-  run-file ysh-builtin-argparse "$@"
-}
-
 ysh-builtin-describe() {
   run-file ysh-builtin-describe "$@"
+}
+
+ysh-builtin-eval() {
+  run-file ysh-builtin-eval "$@"
 }
 
 # Related to errexit-oil
