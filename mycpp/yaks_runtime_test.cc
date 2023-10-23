0fe3016445a58d646ab10487ec3c36f1f2c1bde1
Andy C <andy@oilshell.org>
2023-10-22 20:26:09 -0400

    [mycpp/runtime] Demo for value types (Str_v, value_v, ...)
    This is verbose but it might work.
    
    The cost is accessing every field through self(), and also length()
    wrappers for self()->length_.

--- mycpp/yaks_runtime_test.cc
+++ mycpp/yaks_runtime_test.cc
@@ -8,9 +8,9 @@
 
 #include <new>  // placement new
 
-// #include "mycpp/runtime.h"
 #include "mycpp/common.h"
 #include "mycpp/gc_obj.h"  // ObjHeader
+#include "mycpp/runtime.h"
 #include "vendor/greatest.h"
 
 // namespace yaks {
@@ -34,10 +34,14 @@ These are less than 8 bytes:
 
 Features that mycpp runtime doesn't have:
 
-- Shadow Stack for Garbage Collection
+- More efficient rooting, either:
   - our StackRoots({&a, &b, ...}) is slow
   - our StackRoot takes up a lot of code space!
-  - It's better to spill pointers to a separate frame
+- Either
+  - Shadow Stack for Garbage Collection
+    - It's better to spill pointers to a separate frame
+  - Hybrid rooting
+    - ParamRoot, ParamRoot, SortedPointerRoots,
 
 tagged value =
   Int %Int
@@ -63,6 +67,121 @@ Later:
 
 */
 
+/* Class translation
+
+
+class Slice:
+  def __init__(self, s: str, start: int, length: int):
+    self.s = s
+    self.start = start
+    self.length = length
+
+  def get(self) -> str:
+    return self.s[self.start : self.start + self.length]
+
+
+*/
+
+union yaks_val_t {
+  void* heap_obj;
+  bool b;
+  float f;
+
+  int32_t i;
+  uint64_t u64;  // the whole
+
+  // TODO: where do we put
+  // - 1 byte type_tag?  Or do we have fewer than that?  Maybe 5 bits, since 3
+  // are for small_str len?
+  //   - that's 32 immediate types, and then you can overflow into heap?
+  //   - NO WE only have THREE BITS because our allocator is 24 or 48 byte
+  //   aligned
+  //   - so we have at most 8 immediate values, and the rest heap values?
+  //   - but SmallStr always takes up some space
+
+  // - NUL terminator for SmallStr
+};
+
+class Slice {
+ public:
+  Slice(Str* s, int start, int length) {
+    // TODO: allocate Members
+    self_.heap_obj = Alloc<Members>();
+
+    self()->s_ = s;
+    self()->start_ = start;
+    self()->length_ = length;
+  }
+
+  struct Members {
+    Str* s_;
+    int start_;
+    int length_;
+
+    static constexpr ObjHeader obj_header() {
+      return ObjHeader::ClassFixed(field_mask(), sizeof(Members));
+    }
+    static constexpr uint32_t field_mask() {
+      return maskbit(offsetof(Members, s_));
+    }
+  };
+
+  Members* self() {
+    return static_cast<Members*>(self_.heap_obj);
+  }
+
+  int start() {
+    return self()->start_;
+  }
+
+  // Field Accessors -- this will make the generated code a lot longer?
+  // Naming convention is like protobuf
+
+  int length() {
+    return self()->length_;
+  }
+
+  // Methods
+  Str* Get() {
+    // return StrFromC("yo");
+
+    int n = self()->length_;
+
+    log("start = %d", self()->start_);
+    log("n = %d", n);
+
+    Str* result = NewStr(n);
+
+    memcpy(result->data_, self()->s_->data_ + self()->start_, n);
+
+    result->data_[n] = '\0';
+
+    log("result->data %s", result->data_);
+
+    return result;
+  }
+
+  yaks_val_t self_;
+};
+
+void SliceFunc(Slice myslice) {
+  Str* s = myslice.Get();
+  log("val = %s", s->data_);
+  print(s);
+
+  log("start %d length %d", myslice.start(), myslice.length());
+}
+
+TEST yaks_test() {
+  Slice myslice(StrFromC("hello"), 1, 3);
+
+  log("myslice %p", &myslice);
+
+  SliceFunc(myslice);
+
+  PASS();
+}
+
 // Based on _gen/core/runtime.asdl.h
 
 class value_t {
@@ -77,12 +196,6 @@ class value_t {
   // hnode_t* PrettyTree();
   DISALLOW_COPY_AND_ASSIGN(value_t)
 };
-
-TEST yaks_test() {
-  log("hi = %s", "x");
-
-  PASS();
-}
 
 //}  // namespace small_str_test
 
