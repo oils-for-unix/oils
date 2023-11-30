oils-for-unix/oils

commit 5fe7e6a1cbd2c6392f0372a05c63b550f1df3426

[cpp refactor] Consistently use kEmptyString for EOF
As CPython does.  There's no ambiguity because "\n" is return for an
empty line.

Related to #1759.

cpp/frontend_pyreadline.cc
@@ -299,15 +299,25 @@ BigStr* readline(BigStr* prompt) {
     }
   }
 
-  if (gReadline->latest_line_ != nullptr) {
-    BigStr* s = StrFromC(gReadline->latest_line_);
-    free(gReadline->latest_line_);
-    gReadline->latest_line_ = nullptr;
-    return s;
+  if (gReadline->latest_line_ == nullptr) {
+    // Like Python, EOF is indicated with empty string.
+    return kEmptyString;
   }
-#endif
+  BigStr* s = StrFromC(gReadline->latest_line_);
+  free(gReadline->latest_line_);
+  gReadline->latest_line_ = nullptr;
+  return s;
+#else
+  // Print until first NUL, like print()
+  fputs(prompt->data_, stdout);
 
-  return nullptr;
+  // Plain read from stdin, without GNU readline.
+
+  // Same as pyos::ReadLineBuffered()
+  // For now, test with
+  //    ./configure  --without-readline
+  return mylib::gStdin->readline();
+#endif
 }
 
 }  // namespace py_readline

mycpp/gc_builtins.cc
@@ -431,22 +431,11 @@ int max(List<int>* elems) {
 }
 
 BigStr* raw_input(BigStr* prompt) {
-#ifdef HAVE_READLINE
   BigStr* ret = py_readline::readline(prompt);
-  if (ret == nullptr) {
-    throw Alloc<EOFError>();
-  }
-#else
-  // Same as pyos::ReadLineBuffered()
-  // For now, test with
-  //    ./configure  --without-readline
-  BigStr* ret = mylib::gStdin->readline();
   DCHECK(ret != nullptr);
-
   // Like Python, EOF is indicated with empty string.
   if (len(ret) == 0) {
     throw Alloc<EOFError>();
   }
-#endif
   return ret;
 }

commit a3cdba472aabdd5495575e5bae2e7168793abbfe

[cpp fix] Fix readline() bug when !defined(HAVE_READLINE)
I messed up the interface -- GNU readline doesn't return the trailing
newline, and it uses nullptr to mean EOF.

TODO: spec/stateful was failing, but the CI didn't fail.

Bug introduced in commit

    https://github.com/oilshell/oil/commit/5fe7e6a1cbd2c6392f0372a05c63b550f1df3426

cpp/frontend_pyreadline.cc
@@ -300,8 +300,8 @@ BigStr* readline(BigStr* prompt) {
   }
 
   if (gReadline->latest_line_ == nullptr) {
-    // Like Python, EOF is indicated with empty string.
-    return kEmptyString;
+    // EOF
+    return nullptr;
   }
   BigStr* s = StrFromC(gReadline->latest_line_);
   free(gReadline->latest_line_);

mycpp/gc_builtins.cc
@@ -427,21 +427,32 @@ BigStr* raw_input(BigStr* prompt) {
 #if HAVE_READLINE
   BigStr* ret = py_readline::readline(prompt);
 #else
+  // For now, test this branch with
+  //    ./configure  --without-readline
+
   // Print until first NUL, like print()
   fputs(prompt->data_, stdout);
 
   // Plain read from stdin, without GNU readline.
+  // Similar to pyos::ReadLineBuffered()
 
-  // Same as pyos::ReadLineBuffered()
-  // For now, test with
-  //    ./configure  --without-readline
   BigStr* ret = mylib::gStdin->readline();
+  DCHECK(ret != nullptr);
+
+  // We have to implement the same interface:
+  // - no trailing newline, e.g. so empty line is the empty string
+  // - EOF is nullptr
+  if (len(ret) == 0) {
+    ret = nullptr;
+  } else {
+    ret->RemoveNewlineHack();
+  }
+
 #endif
 
-  DCHECK(ret != nullptr);
-  // Like Python, EOF is indicated with empty string.
-  if (len(ret) == 0) {
+  if (ret == nullptr) {
     throw Alloc<EOFError>();
   }
+  // log("LINE %d [%s]", len(ret), ret->data_);
   return ret;
 }

mycpp/gc_str.cc
@@ -533,6 +533,17 @@ unsigned BigStr::hash(HashFunc h) {
   return hash_;
 }
 
+// for raw_input() to look like GNU readline
+void BigStr::RemoveNewlineHack() {
+  CHECK(data_[len_ - 1] == '\n');
+
+  // This is done before using the string, string can't be hashed
+  DCHECK(is_hashed_ == 0);
+
+  data_[len_ - 1] = '\0';
+  len_--;
+}
+
 static inline BigStr* _StrFormat(const char* fmt, int fmt_len, va_list args) {
   auto beg = std::cregex_iterator(fmt, fmt + fmt_len, gStrFmtRegex);
   auto end = std::cregex_iterator();

mycpp/gc_str.h
@@ -58,6 +58,9 @@ class BigStr {
   BigStr* upper();
   BigStr* lower();
 
+  // for raw_input() to look like GNU readline
+  void RemoveNewlineHack();
+
   // Other options for fast comparison / hashing / string interning:
   // - unique_id_: an index into intern table.  I don't think this works unless
   //   you want to deal with rehashing all strings when the set grows.

test/stateful.sh
@@ -36,7 +36,7 @@ readonly BASE_DIR=_tmp/spec/stateful
 # Hack for testing the harness
 #readonly FIRST='-r 0'
 readonly FIRST=''
-readonly OSH_CPP=_bin/cxx-dbg/osh
+readonly OSH_CPP=_bin/cxx-asan/osh
 
 #readonly -a QUICK_SHELLS=( $OSH bash )
 readonly -a QUICK_SHELLS=( $OSH $OSH_CPP bash )