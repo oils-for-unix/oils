e544a91a2f02018e1a8f38b616dd0df8e5dbc9e5
Andy C <andy@oilshell.org>
2023-12-20 00:13:32 -0500

    [benchmarks/regcomp-cache] Enhance benchmark with comparison
    Surprisingly, OSH isn't slower than bash now?
    
    Even though we are using the same regex 300 times.

--- benchmarks/regcomp-cache/run.sh
+++ benchmarks/regcomp-cache/run.sh
@@ -8,14 +8,20 @@
 # Example:
 #   benchmarks/regcomp-cache/run.sh match-many
 
+set -o nounset
+set -o pipefail
+set -o errexit
 
 match-many() {
-  local num_pat=${1:-1000}
-  local num_str=${1:-1000}
+  local num_pat=${1:-300}
+  local num_str=${1:-300}
+
+  echo BASH_VERSION=${BASH_VERSION:-}
+  echo OILS_VERSION=${OILS_VERSION:-}
 
   declare -a REGEXES=()
   for i in $(seq $num_pat); do
-    REGEXES[i]="$i?$i*$i+"  # last char is modified with ? then * and +
+    REGEXES[i]="$i?($i*)$i+"  # last char is modified with ? then * and +
   done
 
   echo "${REGEXES[@]}"
@@ -23,11 +29,12 @@ match-many() {
   local num_yes=0
   local num_tried=0
 
-  for str in $(seq $num_str); do
-    for i in $(seq $num_pat); do
-      local re="${REGEXES[i]}"
+  for i in $(seq $num_str); do
+    local str="$i$i$i"  # 3 copies
+    for j in $(seq $num_pat); do
+      local re="${REGEXES[j]}"
       if [[ $str =~ $re ]]; then
-        echo "string $str matches pattern $re"
+        echo "string $str matches pattern $re - captured '${BASH_REMATCH[1]}'"
         num_yes=$(( num_yes + 1 ))
       fi
       num_tried=$(( num_tried + 1 ))
@@ -37,6 +44,25 @@ match-many() {
   echo
   echo "num_yes = $num_yes"
   echo "num_tried = $num_tried"
+}
+
+compare() {
+  # must do ./NINJA-config.sh first
+
+  local bin=_bin/cxx-opt/osh
+  ninja $bin
+
+  local dir=_tmp/regcomp-cache
+  mkdir -p $dir
+
+  # with bash
+  { time $0 match-many; } >$dir/bash-stdout.txt 2>$dir/bash-time.txt
+
+  # with OSH
+  { time $bin $0 match-many; } >$dir/osh-stdout.txt 2>$dir/osh-time.txt
+
+  diff -u $dir/*-stdout.txt  # should have equal output
+  head $dir/*-time.txt  # show timings
 }
 
 
