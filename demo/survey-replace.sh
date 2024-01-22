521a79940cb1fc94095da0f3e6d885cb0a0ec6eb
Andy C <andy@oilshell.org>
2024-01-22 00:01:03 -0500

    [demo] Survey of string replace in Python and JS.
    Some things to notice:
    
    - Inconsistency: Python ''.replace() and re.sub() are inconsistent with
      respect to count=0.  String replace does 0 replacements, while
      re.sub() does all replacements.
    
    - JavaScript only replaces the first occurrence of the string.
      - To replace many, it seems like you have to use a regular expression
        with the "g" flag.
    
    - Both Python and JavaScript have the behavior of finding an empty
      string between every character.
      - In Python for both string and regex replacement
      - In JS for regex replacement.  JS string replacement will find one
        empty string at the beginning too.

---
+++ demo/survey-replace.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+#
+# Usage:
+#   data_lang/json-survey.sh <function name>
+
+set -o nounset
+set -o pipefail
+set -o errexit
+
+source build/dev-shell.sh  # python3 in $PATH
+
+string() {
+  echo 'STRING PYTHON'
+  echo
+
+  # This is a float
+  python3 -c 'print("oils-for-unix".replace("i", "++"))'
+
+  # replace none
+  echo 'count=0'
+  python3 -c 'print("oils-for-unix".replace("i", "++", 0))'
+  echo
+
+  # replace all
+  echo 'count=-1'
+  python3 -c 'print("oils-for-unix".replace("i", "++", -1))'
+  echo
+
+  # Very weird empty string behavior -- it finds one between every char
+  python3 -c 'print("oils-for-unix".replace("", "++"))'
+  python3 -c 'print("oils-for-unix".replace("", "++", 1))'
+  echo
+
+  echo 'STRING JS'
+  echo
+  # Only replaces first occurrence!
+  # https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+  nodejs -e 'console.log("oils-for-unix".replace("i", "++"))'
+
+  nodejs -e 'console.log("oils-for-unix".replace("", "++"))'
+}
+
+regex() {
+  echo 'REGEX PYTHON'
+  echo
+
+  python3 -c 'import re; p = re.compile("[i]"); print(p.sub("++", "oils-for-unix"))'
+
+  echo 'count=0 INCONSISTENT, replaces all'
+  python3 -c 'import re; p = re.compile("[i]"); print(p.sub("++", "oils-for-unix", count=0))'
+  echo
+
+  echo 'count=-1'
+  python3 -c 'import re; p = re.compile("[i]"); print(p.sub("++", "oils-for-unix", count=-1))'
+  echo
+
+  # empty string?
+  # It's consistent, it finds empty string between every char
+  python3 -c 'import re; p = re.compile(""); print(p.sub("++", "oils-for-unix"))'
+  echo
+
+  echo 'REGEX JS'
+  echo
+
+  # Replaces first one
+  nodejs -e 'console.log("oils-for-unix".replace(/[i]/, "++"))'
+
+  # Replaces all
+  # no count param?
+  nodejs -e 'console.log("oils-for-unix".replace(/[i]/g, "++"))'
+
+  # Empty regex
+  nodejs -e 'console.log("oils-for-unix".replace(new RegExp(""), "++"))'
+
+  # Hm this is inconsistent -- empty string gets replaced everywhere
+  nodejs -e 'console.log("oils-for-unix".replace(new RegExp("", "g"), "++"))'
+}
+
+"$@"
