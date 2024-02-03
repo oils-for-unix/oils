1cf525a8571a24b1ffbddffcb3b7495105c50c70
Aidan Olsen <a.olsen2024@icloud.com>
2024-02-03 11:40:16 -0700

    mini-argparse (from zulip conversation)

--- spec/ysh-builtin-ctx.test.sh
+++ spec/ysh-builtin-ctx.test.sh
@@ -56,3 +56,56 @@ json write (p)
   ]
 }
 ## END
+
+#### mini-argparse
+proc parser (; place ; ; block_def) {    # place for parser, and block arg
+  var p = {}  # "flag spec", maybe call it parser now
+  ctx push (p) {
+    eval (block_def)
+    # the eval "expands" to calls like this:
+    # ctx emit flag ({short_name: '-v', long_name: '--verbose'})  # flag -v --verbose
+    # ctx emit flag ({long_name: '--count', type: Int, help: 'z'})  # flag --count (Int, help='z')
+    # ctx emit arg (name: 'src')  # arg src
+  }
+  call place->setValue(p)  # "return" the parser we constructed
+}
+
+proc flag (short_name, long_name; type; help) {
+  ctx emit flag ({short_name, long_name, type, help})  # using "punning"
+}
+
+proc arg (name) {
+  ctx emit arg ({name})
+}
+
+var Bool = "Bool"
+
+parser (&spec) {  # call proc parser with place and block
+  flag -t --tsv (Bool, help='')
+  flag -r --rusage (Bool, help='')
+  arg file
+}
+json write (spec)
+## STDOUT:
+{
+  "flag": [
+    {
+      "short_name": "-t",
+      "long_name": "--tsv",
+      "type": "Bool",
+      "help": ""
+    },
+    {
+      "short_name": "-r",
+      "long_name": "--rusage",
+      "type": "Bool",
+      "help": ""
+    }
+  ],
+  "arg": [
+    {
+      "name": "file"
+    }
+  ]
+}
+## END
