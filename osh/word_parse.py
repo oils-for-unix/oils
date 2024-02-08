e829f95f353962571f837ef44ad80ee6dd9f86b7
bar-g <105970722+bar-g@users.noreply.github.com>
2024-02-08 19:46:10 +0000

    word_parse.py: consistent parse_backslash error + hint

--- osh/word_parse.py
+++ osh/word_parse.py
@@ -667,7 +667,7 @@ class WordParser(WordEmitter):
 
                 # x = $'\z' is disallowed; ditto for echo $'\z' if shopt -u parse_backslash
                 if is_ysh_expr or not self.parse_opts.parse_backslash():
-                    p_die("Invalid char escape in C-style string literal", tok)
+                    p_die("Invalid char escape in C-style string literal. Literal: '\\\\' (parse_backslash)", tok)
 
                 tokens.append(tok)
 
@@ -944,7 +944,7 @@ class WordParser(WordEmitter):
                         if (is_ysh_expr or
                                 not self.parse_opts.parse_backslash()):
                             p_die(
-                                "Invalid char escape in double quoted string",
+                                "Invalid char escape in double quoted string. Literal: '\\\\' (parse_backslash)",
                                 self.cur_token)
                     elif self.token_type == Id.Lit_Dollar:
                         if is_ysh_expr or not self.parse_opts.parse_dollar():
@@ -1611,7 +1611,7 @@ class WordParser(WordEmitter):
             ch = lexer.TokenSliceLeft(tok, 1)
             if not self.parse_opts.parse_backslash():
                 if not pyutil.IsValidCharEscape(ch):
-                    p_die('Invalid char escape (parse_backslash)',
+                    p_die("Invalid char escape. Literal: '\\\\' (parse_backslash)',
                           self.cur_token)
 
             part = word_part.EscapedLiteral(self.cur_token,
