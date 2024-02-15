f854d52d62448902a28448b59682afba09534fce
Andy C <andy@oilshell.org>
2024-02-15 18:50:54 -0500

    [test/spec] Re-organize code in spec/testdata
    Anything with read --line conflicts with the spec test framework, which
    reads code on stdin.
    
    j8-read.sh doesn't use read --line, so it can be inlined.

--- spec/ysh-json.test.sh
+++ spec/ysh-json.test.sh
@@ -461,9 +461,37 @@ b'\u{1}\yff\u{1f}'
 
 #### json8 read
 
-# Avoid conflict on stdin from spec test framework?
+echo '{ }' | json8 read
+pp line (_reply)
 
-$SH $REPO_ROOT/spec/testdata/j8-read.sh
+echo '[ ]' | json8 read
+pp line (_reply)
+
+echo '[42]' | json8 read
+pp line (_reply)
+
+echo '[true, false]' | json8 read
+pp line (_reply)
+
+echo '{"k": "v"}' | json8 read
+pp line (_reply)
+
+echo '{"k": null}' | json8 read
+pp line (_reply)
+
+echo '{"k": 1, "k2": 2}' | json8 read
+pp line (_reply)
+
+echo "{u'k': {b'k2': null}}" | json8 read
+pp line (_reply)
+
+echo '{"k": {"k2": "v2"}, "k3": "backslash \\ \" \n line 2 \u03bc "}' | json8 read
+pp line (_reply)
+
+json8 read (&x) <<'EOF'
+{u'k': {u'k2': u'v2'}, u'k3': u'backslash \\ \" \n line 2 \u{3bc} '}
+EOF
+pp line (x)
 
 ## STDOUT:
 (Dict)   {}
--- spec/ysh-user-feedback.test.sh
+++ spec/ysh-user-feedback.test.sh
@@ -51,53 +51,7 @@ test_func
 
 #### Julia port
 
-# https://lobste.rs/s/ritbgc/what_glue_languages_do_you_use_like#c_nhikri
-#
-# See bash counterpart in spec/blog1.test.sh
-
-git-branch-merged() {
-  cat <<EOF
-  foo
-* bar
-  baz
-  master
-EOF
-}
-
-# With bash-style readarray.  The -t is annoying.
-git-branch-merged | while read --line {
-  # Note: this can't be 'const' because const is dynamic like 'readonly'.  And
-  # we don't have block scope.
-  var line = _reply->trim()  # removing leading space
-
-  # with glob: line ~~ '\**'           (awkward)
-  # with regex: line ~ / %start '*' /  (not terrible, but somewhat complex)
-
-  if (line !== 'master' and not line->startsWith('*')) {
-    echo $line
-  }
-} | readarray -t :branches
-
-# TODO: I think we want read --lines :branches ?  Then we don't need this
-# conversion.
-var branchList = :| "${branches[@]}" |
-
-if (len(branchList) === 0) {
-  echo "No merged branches"
-} else {
-  write git branch -D @branchList
-}
-
-# With "append".  Hm read --lines isn't bad.
-var branches2 = :| |
-git-branch-merged | while read --line {
-  var line2 = _reply->trim()  # removing leading space
-  if (line2 !== 'master' and not line2->startsWith('*')) {
-    append $line2 (branches2)
-  }
-}
-
-write -- ___  @branches2
+$SH $REPO_ROOT/spec/testdata/ysh-user-feedback.sh
 
 ## STDOUT:
 git
