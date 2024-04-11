53b71a86bb73a80ccf9ad1fe6e5a402ea0724556
Aidan Olsen <a.olsen2024@icloud.com>
2024-04-11 17:39:40 -0600

    CI doesn't support clang-format, don't fail the test gen script if it is missing

--- data_lang/utf8_decoder_tests_gen.sh
+++ data_lang/utf8_decoder_tests_gen.sh
@@ -2,4 +2,9 @@
 
 mkdir -p _gen/test/utf8
 python3 data_lang/utf8_decoder_tests_gen.py >_gen/test/utf8/decoder-exhaustive.inc
-clang-format -i _gen/test/utf8/decoder-exhaustive.inc
+
+# Try to format, but clang-format isn't always available (eg. CI)
+if ! clang-format -i _gen/test/utf8/decoder-exhaustive.inc; then
+  echo 'Failed to run clang-format on generated decoder tests.'
+  echo 'Make sure it is installed if you want _gen/test/utf8/decoder-exhaustive.inc to be human readable.'
+fi
