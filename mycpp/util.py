149ad95ac7a3066a4729aa831e6ed03ce96a656b
Melvin Walls <mwalls67@gmail.com>
2024-05-27 11:29:47 -0700

    mycpp: avoid subclass name colissions
    Subclass names can collide, e.g. `builtin.func_misc.Type` and
    `builtin.meta_osh.Type` which share the base `core.vm._Callable`. This
    isn't a big deal until you need to query the subclass->base
    relationship, which a future commit will do.
    
    This commit also changes `pass_state.Virtual.virtuals` into a mapping
    from (class, method) -> (base, method). If an entry maps to `None` that
    means the given method is part of the base class. This will allow a
    future commit to find the base for a given subclass.
    
    The commit also changes mycpp to use fully-qualified class names when
    interacting with `pass_state.Virtual` to avoid ambiguity.

--- mycpp/util.py
+++ mycpp/util.py
@@ -19,3 +19,21 @@ def log(msg: str, *args: Any) -> None:
     if args:
         msg = msg % args
     print(msg, file=sys.stderr)
+
+def join_name_cpp(parts: tuple[str], strip_package:bool=False) -> str:
+    if not strip_package:
+        return '::'.join(parts)
+
+    if len(parts) > 1:
+        return '::'.join(('',) + parts[1:])
+
+    return parts[0]
+
+def split_py_name(name: str) -> tuple[str]:
+    ret = tuple(name.split('.'))
+    if len(ret) and ret[0] == 'mycpp':
+        # Drop the prefix 'mycpp.' if present. This makes names compatible with
+        # the examples that use testpkg.
+        return ret[1:]
+
+    return ret
