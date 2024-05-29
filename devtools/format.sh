fcd12ff2d3a600834d41d1ef3456c7eb2bfecd77
Andy C <andy@oilshell.org>
2024-05-29 14:29:18 -0400

    [cleanup] Auto-format lazylex/ dir

--- devtools/format.sh
+++ devtools/format.sh
@@ -60,7 +60,7 @@ yapf-known() {
   ### yapf some files that have been normalized
 
   time yapf-files \
-    {asdl,benchmarks,builtin,core,data_lang,doctools,frontend,mycpp,mycpp/examples,osh,spec/*,yaks,ysh}/*.py \
+    {asdl,benchmarks,builtin,core,data_lang,doctools,frontend,lazylex,mycpp,mycpp/examples,osh,spec/*,yaks,ysh}/*.py \
     */NINJA_subgraph.py
 }
 
@@ -89,7 +89,7 @@ docstrings() {
   #time test/lint.sh py2-files-to-lint \
   #  | xargs --verbose -- python3 -m docformatter --in-place
 
-  python3 -m docformatter --in-place doctools/*.py
+  python3 -m docformatter --in-place lazylex/*.py
 }
 
 #
