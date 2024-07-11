2ca5d8d2b0fd772a02d926cf195c397ccd7187eb
Aidan Olsen <a.olsen2024@icloud.com>
2024-07-10 21:40:21 -0600

    Add some spec tests

--- spec/ysh-builtin-eval.test.sh
+++ spec/ysh-builtin-eval.test.sh
@@ -1,7 +1,7 @@
 # YSH specific features of eval
 
 ## our_shell: ysh
-## oils_failures_allowed: 1
+## oils_failures_allowed: 6
 
 #### Eval does not take a literal block - can restore this later
 
@@ -96,4 +96,89 @@ p {
 ## status: 1
 ## STDOUT:
 TODO
+## END
+
+#### eval with argv bindings
+eval ^[echo "$@"] (argv=:| foo bar baz |)
+eval ^[pp line (:| $1 $2 $3 |)] (argv=:| foo bar baz |)
+
+## status: 0
+## STDOUT:
+foo bar baz
+(List)   ["foo","bar","baz"]
+## END
+
+#### eval with vars bindings
+var myVar = "abc"
+eval (^(pp line (myVar)))
+eval (^(pp line (myVar)), vars={ 'myVar': '123' })
+
+# eval doesn't modify it's environment
+eval (^(pp line (myVar)))
+
+## status: 0
+## STDOUT:
+abc
+123
+## END
+
+#### dynamic binding names and mutation
+proc foreach (binding, in_; list ;; block) {
+  if (in_ !== "in") {
+    error 'Must use the "syntax" `foreach <binding> in (<expr>) { ... }`'
+  }
+
+  for _ in (list) {
+    eval (block, vars={ binding: _ })
+  }
+}
+
+var mydicts = [{'a': 1}, {'b': 2}, {'c': 3}]
+foreach mydict in (mydicts) {
+  pp line (mydict)
+  setvar mydict.d = 0
+}
+
+pp line (mydicts)
+
+## status: 0
+## STDOUT:
+(Dict)   {"a":1}
+(Dict)   {"b":2}
+(Dict)   {"c":3}
+(List)   [{"a":1,"d":0},{"b":2,"d":0},{"c":3,"d":0}]
+## END
+
+#### binding procs in the eval-ed namespace
+proc __flag (short, long) {
+  echo "flag $short $long"
+}
+
+proc __arg (name) {
+  echo "arg $name"
+}
+
+proc parser (; spec ;; block) {
+  eval (block, vars={ 'flag': __flag, 'arg': __arg })
+}
+
+parser (&spec) {
+  flag -h --help
+  arg file
+}
+
+## status: 0
+## STDOUT:
+flag -h --help
+arg file
+## END
+
+#### vars initializes the variable frame, but does not remember it
+var vars = { 'foo': 123 }
+eval (^(var bar = 321), vars=vars)
+pp line (vars)
+
+## status: 0
+## STDOUT:
+(Dict)   {"foo":123}
 ## END
