32fabcc9a869b87c0c8e046b3faeec627b2ddb4e
Andy C <andy@oilshell.org>
2024-11-09 15:27:43 -0500

    [benchmarks] Remove extra arg
    It was always superfluous, but commit
    ebee91222288b98528e84672f84d91a3bd92c81e on mycpp-souffle exposed an
    existing bug.

--- benchmarks/auto.sh
+++ benchmarks/auto.sh
@@ -38,7 +38,7 @@ measure-shells() {
 
   raw_out_dir="$out_dir/osh-runtime/raw.$host_job_id"
   benchmarks/osh-runtime.sh measure \
-    $host_name $raw_out_dir $OSH_CPP_BENCHMARK_DATA $out_dir
+    $host_name $raw_out_dir $OSH_CPP_BENCHMARK_DATA
 
   # Old style uses provenance.txt.  TODO: use raw_out_dir everywhere
   local provenance=_tmp/provenance.txt
