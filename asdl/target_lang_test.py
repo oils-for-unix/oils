6c17a1607943a7a9c70fd1ce9f119916e8343148
Andy C <andy@oilshell.org>
2024-11-18 20:34:24 -0500

    many types could benefit from this

--- asdl/target_lang_test.py
+++ asdl/target_lang_test.py
@@ -2,6 +2,67 @@
 """target_lang_test.py: test out MyPy-compatible code we generated
 
 Similar to mycpp/demo/target_lang.cc
+
+List of types that could be containers:
+
+syntax.asdl:
+
+  CompoundWord = (List[word_part] parts)
+  
+  word_part = 
+      ...
+    | BracedTuple(List[CompoundWord] words)
+  
+  condition = 
+      Shell(List[command] commands)  # if false; true; then echo hi; fi
+  
+  pat =
+    Else
+  | Words(List[word] words)
+  | YshExprs(List[expr] exprs)
+  
+  command =
+    | CommandList(List[command] children)
+  
+  re = 
+    | Seq(List[re] children)
+    | Alt(List[re] children)
+
+
+runtime.asdl
+
+  part_value = 
+    String %Piece
+
+    # "$@" or "${a[@]}" # never globbed or split (though other shells
+    # split them)
+  | Array(List[str] strs)
+  | ExtGlob(List[part_value] part_vals)
+
+  wait_status =
+  | Pipeline(List[int] codes)
+
+  trace =
+    External(List[str] argv) # sync, needs argv (command.Simple or 'command')
+
+value.asdl
+
+  value =
+    | Str(str s)
+  
+      # Maybe we can add global singletons value::Bool::true and
+      # value::Bool::false or something?  They are pointers.
+    | Bool(bool b)
+  
+    # Not sure if we can do anything about these.  Probably should for Int and
+    # Float
+    | Int(BigInt i)
+    | Float(float f)
+  
+    | BashAssoc(Dict[str, str] d)
+    | List(List[value] items)
+    | Dict(Dict[str, value] d)
+    | Frame(Dict[str, Cell] frame)
 """
 
 from typing import List, Dict
