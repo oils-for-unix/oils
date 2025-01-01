7c12d0478b6c28220cf76126fd3a6a1746a464b4
Koichi Murase <myoga.murase@gmail.com>
2025-01-01 22:11:54 +0900

    [osh] Implement SparseArray `$sp` as `${sp[0]}`

--- spec/ble-idioms.test.sh
+++ spec/ble-idioms.test.sh
@@ -1211,6 +1211,29 @@ a3 empty: [ ]
 ## END
 
 
+#### SparseArray: ${a-}
+case $SH in zsh|mksh|ash) exit ;; esac
+
+a1=()
+a2=("" "")
+a3=(foo bar)
+
+case ${SH##*/} in osh) eval 'var a1 = _a2sp(a1); var a2 = _a2sp(a2); var a3 = _a2sp(a3)' ;; esac
+
+echo "$a1, ${a1-(unset)}, ${a1:-(empty)};"
+echo "$a2, ${a2-(unset)}, ${a2:-(empty)};"
+echo "$a3, ${a3-(unset)}, ${a3:-(empty)};"
+
+## STDOUT:
+, (unset), (empty);
+, , (empty);
+foo, foo, foo;
+## END
+
+## N-I zsh/mksh/ash STDOUT:
+## END
+
+
 #### SparseArray: compgen -F _set_COMPREPLY
 case $SH in zsh|mksh|ash) exit ;; esac
 
