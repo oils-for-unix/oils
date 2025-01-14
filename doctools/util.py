e7e02b55c8e13873782ba58e2f0e5b8693a594df
Andy C <andy@oilshell.org>
2025-01-14 15:28:34 -0500

    [doctools] Fix PYTHONPATH issue
    doctools/ul_table_test.py runs under both Python 2 and Python 3.
    
    vendor/typing.py is Python 2 only.  Because Python 3 has typing in the
    stdlib.
    
    So we need the regular 'import typing', which is better anyway.
    
    ---
    
    Note: we could also put pyannotate, mypy_extensions, and six in vendor/
    !
    
    And maybe an old version of pygments.

--- doctools/util.py
+++ doctools/util.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python2
-"""util.py."""
 from __future__ import print_function
 
 import sys
 
-# Note: from typing import Any causes ImportError when PYTHONPATH is not .:vendor
-# So we import from vendor.typing which is a little inconsistent
-from vendor.typing import Any
+from typing import Any
 
 
 def log(msg, *args):
