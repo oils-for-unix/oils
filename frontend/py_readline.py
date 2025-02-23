f351645120c27c0a33408119505f7f4e8ba4d195
Matthew Davidson <matthew@modulolotus.net>
2025-02-23 17:39:51 +0700

    Basic bind -x c++ support

--- frontend/py_readline.py
+++ frontend/py_readline.py
@@ -140,7 +140,7 @@ class Readline(object):
 
     def unbind_keyseq(self, keyseq):
         # type: (str) -> None
-        line_input.unbind_keyseq(keyseq)
+        line_input.unbind_keyseq(keyseq)    
 
     def bind_shell_command(self, bindseq):
         # type: (str) -> None
