d54b3086471cc724a0c8d9599f6e586a2bf27889
Koichi Murase <myoga.murase@gmail.com>
2025-03-02 15:38:18 +0900

    [osh/cmd_eval] Use tagswitch

--- builtin/assign_osh.py
+++ builtin/assign_osh.py
@@ -20,7 +20,7 @@ from core import vm
 from display import ui
 from frontend import flag_util
 from frontend import args
-from mycpp.mylib import log
+from mycpp.mylib import log, tagswitch
 from osh import cmd_eval
 from osh import sh_expr_eval
 from data_lang import j8_lite
@@ -242,29 +242,33 @@ def _AssignVarForBuiltin(mem, rval, pair, which_scopes, flags, arith_ev,
                     "Can't convert type %s into BashArray" %
                     ui.ValType(old_val), pair.blame_word)
         elif flag_A:
-            if old_val.tag() == value_e.Undef:
-                # Note: We explicitly initialize BashAssoc for Undef.
-                val = bash_impl.BashAssoc_New()
-            elif old_val.tag() == value_e.Str:
-                # Note: We explicitly initialize BashAssoc for Str.  When
-                #   applying +=() to Str, we associate an old value to the key
-                #   '0'.  OSH disables this when strict_array is turned on.
-                assoc_val = bash_impl.BashAssoc_New()
-                if pair.plus_eq:
-                    if mem.exec_opts.strict_array():
-                        e_die("Can't convert Str to BashAssoc (strict_array)",
-                              pair.blame_word)
-                    bash_impl.BashAssoc_SetElement(assoc_val, '0',
-                                                   cast(value.Str, old_val).s)
-                val = assoc_val
-            elif old_val.tag() == value_e.BashAssoc:
-                # We do not need adjustments for -A.
-                pass
-            else:
-                # Note: BashArray cannot be converted to a BashAssoc
-                e_die(
-                    "Can't convert type %s into BashAssoc" %
-                    ui.ValType(old_val), pair.blame_word)
+            with tagswitch(old_val) as case:
+                if case(value_e.Undef):
+                    # Note: We explicitly initialize BashAssoc for Undef.
+                    val = bash_impl.BashAssoc_New()
+                elif case(value_e.Str):
+                    # Note: We explicitly initialize BashAssoc for Str.  When
+                    #   applying +=() to Str, we associate an old value to the
+                    #   key '0'.  OSH disables this when strict_array is turned
+                    #   on.
+                    assoc_val = bash_impl.BashAssoc_New()
+                    if pair.plus_eq:
+                        if mem.exec_opts.strict_array():
+                            e_die(
+                                "Can't convert Str to BashAssoc (strict_array)",
+                                pair.blame_word)
+                        bash_impl.BashAssoc_SetElement(
+                            assoc_val, '0',
+                            cast(value.Str, old_val).s)
+                    val = assoc_val
+                elif case(value_e.BashAssoc):
+                    # We do not need adjustments for -A.
+                    pass
+                else:
+                    # Note: BashArray cannot be converted to a BashAssoc
+                    e_die(
+                        "Can't convert type %s into BashAssoc" %
+                        ui.ValType(old_val), pair.blame_word)
 
         val = cmd_eval.ListInitializeTarget(val, initializer, pair.plus_eq,
                                             mem.exec_opts, pair.blame_word)
