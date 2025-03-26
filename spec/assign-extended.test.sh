oils-for-unix/oils

commit 07e185920ab68ab06924e39c81312ae6f5b1c9a6

[builtin cleanup] Minor code improvements
- Remove conditional
- Add assertion

builtin/assign_osh.py
@@ -116,21 +116,19 @@ def _PrintVariables(
     for name in names:
         cell = cells[name]
         if cell is None:
-            if builtin == _OTHER:
-                # This is specified for declare/typeset -p x y
-                # Notes:
-                #   local -p behaves differently in bash 4 and bash 5!
-                #   location info?
-                errfmt.PrintMessage(
-                    'osh: %s: %r is not defined' % (cmd_val.argv[0], name),
-                    cmd_val.arg_locs[0])
+            # declare/typeset/local -p var1 var2 print an error
+            # There is no readonly/export -p var1 var2
+            errfmt.PrintMessage(
+                'osh: %s: %r is not defined' % (cmd_val.argv[0], name),
+                cmd_val.arg_locs[0])
             continue  # not defined
 
         val = cell.val
+        # Mem.var_stack does not store value.Undef
+        assert val.tag() != value_e.Undef, val
+
         #log('name %r %s', name, val)
 
-        if val.tag() == value_e.Undef:  # TODO: this can be an assertion
-            continue
         if builtin == _READONLY and not cell.readonly:
             continue
         if builtin == _EXPORT and not cell.exported:

commit 88d9ef7a35049eeae854fbf2426b077436450601

[builtin/declare] Fix crash bug
Introduced in https://github.com/oils-for-unix/oils/commit/07e185920ab68ab06924e39c81312ae6f5b1c9a6

Found by Koichi

This also exposed an incompatibility, which we can fix later.

builtin/assign_osh.py
@@ -124,10 +124,10 @@ def _PrintVariables(
             continue  # not defined
 
         val = cell.val
-        # Mem.var_stack does not store value.Undef
-        assert val.tag() != value_e.Undef, val
 
-        #log('name %r %s', name, val)
+        # Mem.var_stack CAN store value.Undef
+        if val.tag() == value_e.Undef:
+            continue
 
         if builtin == _READONLY and not cell.readonly:
             continue

spec/assign-extended.test.sh
@@ -1,4 +1,5 @@
 ## compare_shells: bash-4.4 mksh
+## oils_failures_allowed: 1
 
 # Extended assignment language, e.g. typeset, declare, arrays, etc.
 # Things that dash doesn't support.
@@ -574,6 +575,27 @@ arr[3]=a10
 ## END
 ## N-I mksh stdout-json: ""
 ## N-I mksh status: 1
+
+#### declare -p and value.Undef
+
+# This is a regression for a crash
+# But actually there is also an incompatibility -- we don't print anything
+
+declare x
+declare -p x
+
+function f { local x; declare -p x; }
+x=1
+f
+
+## STDOUT:
+declare -- x
+declare -- x
+## END
+
+## N-I mksh status: 127
+## N-I mksh STDOUT:
+## END
 
 #### eval -- "$(declare -p arr)" (restore arrays w/ unset elements)
 arr=(1 2 3)