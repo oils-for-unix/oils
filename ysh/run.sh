cb49aa0cc3e16ca13722ad404790c61f17aed5ea
Andy C <andy@oilshell.org>
2025-04-25 11:59:34 -0400

    rename ysh

--- ysh/run.sh
+++ ysh/run.sh
@@ -9,8 +9,7 @@ set -o errexit
 
 source test/common.sh
 
-# TODO: Rename to ysh
-OIL=${OIL:-'bin/oil'}
+YSH=${YSH:-'bin/ysh'}
 OSH_CPP=_bin/cxx-asan/osh
 # Assertion failed
 #OSH_CPP=_bin/cxx-dbg/osh
@@ -71,7 +70,7 @@ test-run-ysh() {
 
   for prog in ysh/testdata/*.ysh; do
     echo ---
-    $OIL $prog all
+    $YSH $prog all
   done
 }
 
