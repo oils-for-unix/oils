47f5110a184a98b7fb8234bcd02cae3f0c2fedbd
Andy C <andy@oils.pub>
2025-07-19 22:01:05 -0400

    [test/aports] Able to build packages inside the chroot
    with 'abuild -r'
    
    Needed to set up
    
    - abuild-keygen
    - which requires setting up the 'doas' tool

--- test/aports.sh
+++ test/aports.sh
@@ -2,6 +2,21 @@
 #
 # Usage:
 #   test/aports.sh <function name>
+#
+# Examples:
+#   $0 clone-aports
+#   $0 clone-ci
+#   $0 make-chroot
+#   $0 install-packages   # build packages we need
+#   $0 make-user
+#   $0 setup-doas
+#   $0 build-packages
+#
+# To clean up:
+#
+#   _chroot/aports-build/destroy --remove
+# 
+# This unmounts /dev /proc /sys/ properly!
 
 : ${LIB_OSH=stdlib/osh}
 source $LIB_OSH/bash-strict.sh
@@ -64,7 +79,25 @@ make-chroot() {
 }
 
 make-user() {
-  _chroot/aports-build/enter-chroot adduser -D builder
+  _chroot/aports-build/enter-chroot adduser -D builder || true
+
+  # put it in abuild group
+  _chroot/aports-build/enter-chroot addgroup builder abuild || true
+
+  # for sudo
+  _chroot/aports-build/enter-chroot addgroup builder wheel || true
+
+  _chroot/aports-build/enter-chroot -u builder sh -c 'whoami; echo GROUPS; groups'
+}
+
+setup-doas() {
+  # Manual configuration for abuild-keygen
+
+  #sudo cat _chroot/aports-build/etc/doas.conf
+  sudo rm _chroot/aports-build/etc/doas.conf
+
+  # no password
+  _chroot/aports-build/enter-chroot sh -c 'echo "permit nopass :wheel" >> /etc/doas.conf'
 }
 
 install-packages() {
@@ -74,17 +107,90 @@ install-packages() {
   #return
 
   # Must be done as root; there is no 'sudo'
-  _chroot/aports-build/enter-chroot sh -c 'apk update; apk add bash'
-  _chroot/aports-build/enter-chroot -u builder bash -c 'echo "hi from bash"'
+  # doas: for abuild-keygen?
+  _chroot/aports-build/enter-chroot sh -c 'apk update; apk add bash abuild alpine-sdk doas'
 
+  _chroot/aports-build/enter-chroot -u builder bash -c 'echo "hi from bash"'
   #_chroot/aports-build/enter-chroot -u builder sh -c 'apk update; apk add bash'
 }
 
-# TODO:
-# - make a regular non-root user
-# - build a package with plain 'abuild'?
-# - install abuild-rootbld ?
-# - install buildrepo?
+copy-aports() {
+  local dest=_chroot/aports-build/home/builder/aports/main/
+
+  sudo mkdir -p $dest
+  sudo rsync --archive --verbose \
+    ../../alpinelinux/aports/main/ $dest
+
+  # get uid from /home/builder
+  local uid
+  uid=$(stat -c '%u' _chroot/aports-build/home/builder/)
+  sudo chown --verbose --recursive $uid $dest
+}
+
+abuild-conf() {
+  echo  
+}
+
+keys() {
+  _chroot/aports-build/enter-chroot -u builder bash -c '
+  abuild-keygen -h
+
+  file $(which abuild-keygen)
+  cat $(which abuild-keygen)
+  # 186 line shell script
+  wc -l $(which abuild-keygen)
+
+  #exit
+
+  #sh -x $(which abuild-keygen) --append --install
+  abuild-keygen --append --install
+  '
+}
+
+build-packages() {
+
+  # Hm says abuild -r
+  # https://wiki.alpinelinux.org/wiki/Abuild_and_Helpers#Basic_usage
+  #
+  # But Claude gave me abuild -F deps unpack
+
+
+  _chroot/aports-build/enter-chroot -u builder bash -c '
+  abuild --help
+
+  ls -l /var/cache
+  ls -l /var/cache/distfiles
+
+  #dir=aports/main/bash
+
+  # Try building lua
+  #dir=aports/main/lua5.2
+  dir=aports/main/mpfr4
+
+  ls -l $dir
+
+  pushd $dir
+  #abuild fetch #verify
+  #abuild unpack
+  #abuild build -r
+
+  #abuild -F deps unpack prepare build install package
+
+  abuild -r
+
+  popd
+
+  exit
+  abuild-keygen -h
+  newapkbuild -h
+  #apkbuild-pypi -h
+  buildrepo -h
+  '
+}
+
+# Notes:
+# - buildrepo.lua is a lua script in lua-aports
+# - abuild rootbld make a chroot for each package?
 #
 # Then 
 # - install OSH as /bin/sh
