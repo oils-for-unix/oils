3b4079988bf5665521d36398c8aead280a8e8ae2
Andy C <andy@oils.pub>
2025-08-06 15:39:21 -0400

    [regtest/bwrap] Got rootless bwrap working
    Prior to this change, we used the same strategy as alpine-chroot-install
    
    - bwrap, wrapping 'su -l udu'
      - this starts a login shell, which should read /etc/profile
      - it also sets USER LOGNAME HOME SHELL
    
    Now what we do is:
    
    - Given the user name, use awk to parse (uid, gid, home, shell) out of
      $CHROOT_DIR/etc/passwd
    - Then pass flags to bwrap
      * --uid and --gid
      * --clearenv --setenv HOME ...
    
    This works nicely!  We have 11 env vars in the guest shell, just as we
    did before.
    
    The next thing would be to start a login shell by invoking with argv0
    starting with '-'.  We could use bash 'exec -a', or we could use a
    symlink.

--- regtest/enter-bwrap.sh
+++ regtest/enter-bwrap.sh
@@ -2,7 +2,8 @@
 #
 # Enter an Alpine rootfs with bwrap, not chroot.
 #
-# This
+# This is an alternative to 'enter-chroot'.  It's not a "hook", like
+# regtest/enter-hook-{bwrap,chroot}
 
 set -e
 
@@ -24,15 +25,82 @@ bwrap_flags=${3:-}
 
 shift 3
 
-# note:
-# - su must be root to work
-# - I'd like it unprivileged
+# this prints the line, but it's hard to parse in shell
+#  getent passwd "$user"
 
-echo RUN "$@"
-set -x
+# this seems like the most reliable way to get
 
-# note weird su syntax, without the shell
-# su -l "$user" -- -c
+temp_prefix="/tmp/$$"
+
+uid_file=$temp_prefix.uid.txt
+gid_file=$temp_prefix.gid.txt
+home_file=$temp_prefix.home.txt
+shell_file=$temp_prefix.shell.txt
+
+awk -F : \
+  -v user=$user \
+  -v uid_file=$uid_file \
+  -v gid_file=$gid_file \
+  -v home_file=$home_file \
+  -v shell_file=$shell_file \
+  '
+  $1 == user {
+    #printf("FOUND User %s in the rootfs\n", user);
+    print $3 > uid_file
+    print $4 > gid_file
+    print $6 > home_file
+    print $7 > shell_file
+    found = 1
+    exit  # jumps to the END block
+  }
+
+  END {
+    if (found == 0) {
+      printf("User %s not found in the rootfs\n", user);
+      exit 1
+    }
+  }
+  ' $rootfs_dir/etc/passwd
+
+# Note: there is a slurp operator in bash/ksh $(<file)
+uid=$(cat $uid_file)
+gid=$(cat $gid_file)
+user_home=$(cat $home_file)
+user_shell=$(cat $shell_file)
+
+# Clean up
+rm $temp_prefix*
+
+if false; then
+  echo uid=$uid
+  echo gid=$gid
+  echo home=$user_home
+  echo shell=$user_shell
+fi
+
+if true; then
+# rootless method
+
+bwrap \
+  $bwrap_flags \
+  --unshare-user \
+  --uid $uid \
+  --gid $gid \
+  --clearenv \
+  --setenv USER $user \
+  --setenv LOGNAME $user \
+  --setenv SHELL $user_shell \
+  --setenv HOME $user_home \
+  --bind "$rootfs_dir" / \
+  --proc /proc \
+  --bind /proc/sys/kernel/overflowuid /proc/sys/kernel/overflowuid \
+  --bind /proc/sys/kernel/overflowgid /proc/sys/kernel/overflowgid \
+  --dev /dev \
+  -- \
+  $user_shell -c '. /etc/profile; "$@"' dummy0 "${@:-sh}"
+
+else
+  # this requires root, because su requires root
 
 bwrap \
   $bwrap_flags \
@@ -44,4 +112,4 @@ bwrap \
   -- \
   /usr/bin/env -i -- \
   su -l "$user" -- -c '. /etc/profile; . /env.sh; "$@"' dummy0 "${@:-sh}"
-
+fi
