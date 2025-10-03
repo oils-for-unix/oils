328b4aad78e25da05afa4248a972d9dc42000228
Aidan Olsen <a.olsen2024@icloud.com>
2025-10-02 21:08:06 -0600

    Add failing spec tests (and also add spec test for bug re: PATH invalidation I found)

--- spec/divergence.test.sh
+++ spec/divergence.test.sh
@@ -1,5 +1,5 @@
 ## compare_shells: bash dash mksh zsh ash
-## oils_failures_allowed: 4
+## oils_failures_allowed: 6
 
 # This file relates to:
 #
@@ -86,4 +86,105 @@ eval false; echo $?
 1
 0
 1
+## END
+
+#### Executing command with same name as directory in PATH (#2429)
+
+# Make the following directory structure. File type and permission bits are
+# given on the left.
+# [drwxr-xr-x]  _tmp
+# +-- [drwxr-xr-x]  bin
+# |   \-- [-rwxr-xr-x]  hello
+# +-- [drwxr-xr-x]  notbin
+# |   \-- [-rw-r--r--]  hello
+# \-- [drwxr-xr-x]  dir
+#     \-- [drwxr-xr-x]  hello
+mkdir -p _tmp/bin
+mkdir -p _tmp/bin2
+mkdir -p _tmp/notbin
+mkdir -p _tmp/dir/hello
+printf '#!/usr/bin/env sh\necho hi\n' >_tmp/notbin/hello
+printf '#!/usr/bin/env sh\necho hi\n' >_tmp/bin/hello
+chmod +x _tmp/bin/hello
+
+DIR=$PWD/_tmp/dir
+BIN=$PWD/_tmp/bin
+NOTBIN=$PWD/_tmp/notbin
+
+# The command resolution will search the path for matching *files* (not
+# directories) WITH the execute bit set.
+
+# Should find executable hello right away and run it
+PATH="$BIN:$PATH" hello
+echo status=$?
+
+hash -r  # Needed to clear the PATH cache
+
+# Will see hello dir, skip it and then find&run the hello exe
+PATH="$DIR:$BIN:$PATH" hello
+echo status=$?
+
+hash -r  # Needed to clear the PATH cache
+
+# Will see hello (non-executable) file, skip it and then find&run the hello exe
+PATH="$NOTBIN:$BIN:$PATH" hello
+echo status=$?
+
+## STDOUT:
+hi
+status=0
+hi
+status=0
+hi
+status=0
+## END
+
+#### Changing PATH will invalidate PATH cache
+
+mkdir -p _tmp/bin
+mkdir -p _tmp/bin2
+printf '#!/usr/bin/env sh\necho hi\n' >_tmp/bin/hello
+printf '#!/usr/bin/env sh\necho hey\n' >_tmp/bin2/hello
+chmod +x _tmp/bin/hello
+chmod +x _tmp/bin2/hello
+
+BIN=$PWD/_tmp/bin
+BIN2=$PWD/_tmp/bin2
+
+# Will find _tmp/bin/hello
+PATH="$BIN:$PATH" hello
+echo status=$?
+
+# Should invalidate cache and then find _tmp/bin2/hello
+PATH="$BIN2:$PATH" hello
+echo status=$?
+
+# Same when PATH= and export PATH=
+PATH="$BIN:$PATH"
+hello
+echo status=$?
+PATH="$BIN2:$PATH"
+hello
+echo status=$?
+
+export PATH="$BIN:$PATH"
+hello
+echo status=$?
+export PATH="$BIN2:$PATH"
+hello
+echo status=$?
+
+## STDOUT:
+hi
+status=0
+hey
+status=0
+hi
+status=0
+hey
+status=0
+hi
+status=0
+hey
+status=0
 ## END
