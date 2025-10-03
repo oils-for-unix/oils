66c0d853d6fd5a676ace51d1114a9ea7e559e5e4
Aidan Olsen <a.olsen2024@icloud.com>
2025-10-02 23:10:20 -0600

    Add mycpp translation of isfile

--- cpp/pylib_test.cc
+++ cpp/pylib_test.cc
@@ -24,6 +24,7 @@ TEST os_path_test() {
   ASSERT(str_equals(s, StrFromC("///")));
 
   ASSERT(path_stat::exists(StrFromC("/")));
+  ASSERT(path_stat::exists(StrFromC("/usr/bin/sh")));
   ASSERT(!path_stat::exists(StrFromC("/nonexistent_ZZZ")));
 
   PASS();
@@ -32,6 +33,18 @@ TEST os_path_test() {
 TEST isdir_test() {
   ASSERT(path_stat::isdir(StrFromC(".")));
   ASSERT(path_stat::isdir(StrFromC("/")));
+  ASSERT(!path_stat::isdir(StrFromC("/usr/bin/sh")));
+  ASSERT(!path_stat::isdir(StrFromC("/nonexistent_ZZZ")));
+
+  PASS();
+}
+
+TEST isfile_test() {
+  ASSERT(!path_stat::isfile(StrFromC(".")));
+  ASSERT(!path_stat::isfile(StrFromC("/")));
+  ASSERT(path_stat::isfile(StrFromC("/usr/bin/sh")));
+  ASSERT(!path_stat::isfile(StrFromC("/nonexistent_ZZZ")));
+
   PASS();
 }
 
@@ -44,6 +57,7 @@ int main(int argc, char** argv) {
 
   RUN_TEST(os_path_test);
   RUN_TEST(isdir_test);
+  RUN_TEST(isfile_test);
 
   gHeap.CleanProcessExit();
 
