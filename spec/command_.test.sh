7dd7848af777cee94355c0bc35a9b253d072055b
Andrii Sultanov <sultanovandriy@gmail.com>
2025-10-24 17:23:17 +0100

    [spec] Add PATH cache invalidation tests
    Move the PATH cache test from divergence.test.sh to command_.test.sh
    
    Add a new test verifying that changing PATH before builtin exec also
    invalidates the PATH cache
    
    Signed-off-by: Andrii Sultanov <sultanovandriy@gmail.com>

--- spec/command_.test.sh
+++ spec/command_.test.sh
@@ -244,3 +244,78 @@ status=0
 hi
 status=0
 ## END
+#
+#### Changing $PATH before external command will invalidate PATH cache
+
+mkdir -p _tmp/bin
+mkdir -p _tmp/bin2
+printf '#!/usr/bin/env sh\necho hi\n' >_tmp/bin/hello
+printf '#!/usr/bin/env sh\necho hey\n' >_tmp/bin2/hello
+chmod +x _tmp/bin/hello
+chmod +x _tmp/bin2/hello
+
+BIN=$PWD/_tmp/bin
+BIN2=$PWD/_tmp/bin2
+
+# Will find _tmp/bin/hello
+PATH="$BIN:$PATH" hello
+echo status=$?
+
+# Should invalidate cache and then find _tmp/bin2/hello
+PATH="$BIN2:$PATH" hello
+echo status=$?
+
+# Same when PATH= and export PATH=
+PATH="$BIN:$PATH"
+hello
+echo status=$?
+PATH="$BIN2:$PATH"
+hello
+echo status=$?
+
+export PATH="$BIN:$PATH"
+hello
+echo status=$?
+export PATH="$BIN2:$PATH"
+hello
+echo status=$?
+
+## STDOUT:
+hi
+status=0
+hey
+status=0
+hi
+status=0
+hey
+status=0
+hi
+status=0
+hey
+status=0
+## END
+
+#### Changing $PATH before exec will invalidate path cache
+
+mkdir -p _tmp/bin
+mkdir -p _tmp/bin2
+printf '#!/usr/bin/env sh\necho hi\n' >_tmp/bin/hello
+printf '#!/usr/bin/env sh\necho hey\n' >_tmp/bin2/hello
+chmod +x _tmp/bin/hello
+chmod +x _tmp/bin2/hello
+
+BIN=$PWD/_tmp/bin
+BIN2=$PWD/_tmp/bin2
+
+# Will find _tmp/bin/hello
+PATH="$BIN:$PATH" hello
+echo status=$?
+
+PATH="$BIN2:$PATH" exec hello
+echo status=$?
+
+## STDOUT:
+hi
+status=0
+hey
+## END
