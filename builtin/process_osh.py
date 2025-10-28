fba8c55f813c0690ea05d3ea4e6130aca99b5150
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2025-10-28 13:06:45 -0700

    fix typing

--- builtin/process_osh.py
+++ builtin/process_osh.py
@@ -506,7 +506,7 @@ class Umask(vm._Builtin):
         e_usage("unexpected number of arguments", loc.Missing)
 
     def _MaskFromClauseList(self, mask, clause_list):
-        # type: (int, list[str]) -> (bool, int)
+        # type: (int, List[str]) -> Tuple[bool, int]
 
         for clause in clause_list:
             ok, mask = self._SymbolicToOctal(mask, clause)
@@ -517,7 +517,7 @@ class Umask(vm._Builtin):
         
     # TODO: update the naming convention here
     def _SymbolicToOctal(self, initial_mask, arg_component):
-        # type: (int, str) -> (bool, int)
+        # type: (int, str) -> Tuple[bool, int]
 
         # TODO: location highlighting would be nice
         if len(arg_component) == 0:
