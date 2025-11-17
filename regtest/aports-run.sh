89cce0d1ae8d55caea35381f5dfb8f0d94256100
Bram Tertoolen <bramtertoolen@gmail.com>
2025-11-17 17:27:49 +0100

    update Rebuild:... echo

--- regtest/aports-run.sh
+++ regtest/aports-run.sh
@@ -312,7 +312,7 @@ build-package-overlayfs() {
 
   if test -n "$INTERACTIVE"; then
     echo "Starting interactive shell in overlayfs environment for package $a_repo/$pkg"
-    echo "Rebuild: abuild -f -r -C ~/aports/$a_repo/$pkg"
+    echo "Rebuild: abuild -f -r -C ~/aports/$a_repo/$pkg -k -K"
     echo "   Help: abuild -h"
     # If the last command in the child shell exited non-zero then ctrl-d/exit
     # will report that error code to the parent. If we don't ignore that error
