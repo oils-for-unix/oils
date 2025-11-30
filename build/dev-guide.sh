0643995b0fe9c9992467043d18ede6c39b66e375
Aidan Olsen <a.olsen2024@icloud.com>
2025-11-29 20:52:26 -0700

    Add dev-guide build script

---
+++ build/dev-guide.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+#
+# Usage:
+#   build/dev-guide.sh build-dev-guide
+#
+# This will clone the oils-for-unix/oils wiki, and build all of the markdown
+# files using doc tools. The docs are saved to _release/VERSION/doc/wiki.
+#
+# This can be used for the dev guide which is located at
+# _release/VERSION/doc/wiki/Dev-Guide.html.
+
+set -e
+
+readonly HTML_BASE_DIR=_release/VERSION
+readonly WIKI_DIR=_tmp/wiki
+
+has-wiki() {
+  test -d "$WIKI_DIR"
+}
+
+clone-wiki() {
+  if has-wiki; then
+    echo "Wiki already cloned to $WIKI_DIR, pulling latest"
+
+    pushd "$WIKI_DIR"
+    git pull
+    popd
+    return
+  fi
+
+  echo "Cloning wiki to $WIKI_DIR"
+  mkdir -p "$WIKI_DIR"
+  git clone https://github.com/oils-for-unix/oils.wiki.git "$WIKI_DIR"
+}
+
+pre-render-wikilinks() {
+  ## GitHub wikis have a unique [[link syntax]] which references topic within
+  ## the wiki.
+  ##
+  ## This function converts that syntax to the traditional
+  ## [link syntax](./link-syntax.html) which will render correctly once fed to
+  ## doctools.
+  python3 <<'EOF'
+import sys
+import re
+
+def slugify(text: str) -> str:
+    """Convert link text to a filename slug."""
+    return text.strip().lower().replace(" ", "-").replace(",", "")
+
+pattern = re.compile(r"\[\[(.*?)\]\]")
+
+def replacer(match):
+    text = match.group(1).strip()
+    slug = slugify(text)
+    return f"[{text}](./{slug}.html)"
+
+input_text = sys.stdin.read()
+output_text = pattern.sub(replacer, input_text)
+sys.stdout.write(output_text)
+EOF
+}
+
+build-one() {
+  local path=$1
+  local name=$(basename "$path")
+  local name=${name%.md}  # Remove .md extension
+  local name=${name/,//}  # Remove commas in names (breaks doctools)
+  local title=$(echo "$name" | sed 's/ /-/g')
+  mkdir -p "$HTML_BASE_DIR/doc/wiki/"
+  local web_url="../../web"
+  build/doc.sh render-only <(pre-render-wikilinks <"$path") "$HTML_BASE_DIR/doc/wiki/$name.html" "$web_url/base.css $eb_url/manual.css $web_url/toc.css $web_url/language.css $web_url/code.css" "$title"
+}
+
+build-dev-guide() {
+  clone-wiki
+  # find "$WIKI_DIR" -name '*.md' -print0 | xargs "-P$(nproc)" -I {} -0 -- $0 build-one {}
+  find "$WIKI_DIR" -name '*.md' -print0 | xargs -P1 -I {} -0 -- $0 build-one {}
+}
+
+"$@"
