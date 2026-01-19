952596432b0c39d612d9ef569ca574d4958c6605
Aidan Olsen <a.olsen2024@icloud.com>
2026-01-19 11:49:46 -0700

    Add failing spec test for #2640

--- spec/pipeline.test.sh
+++ spec/pipeline.test.sh
@@ -1,4 +1,4 @@
-## oils_failures_allowed: 1
+## oils_failures_allowed: 2
 ## compare_shells: bash dash mksh zsh
 
 #
@@ -274,4 +274,28 @@ status=1
 ## END
 
 ## N-I dash STDOUT:
+## END
+
+#### #2640 Hanging when reading from character devices
+# Relies on timeout from GNU coreutils
+timeout 0.5 $SH -c 'cat /dev/zero | cat | head -c 5 | tr \\0 0; echo'
+
+# The exact cause of the issue is:
+#
+#  $ cat /dev/zero | cat | head -c 5
+#
+# A pipe through cat is necessary and the first cat MUST be reading from a
+# character device.
+
+# Other cases without the bug above
+printf '\0\0\0\0\0\0\0' > tmp.txt
+$SH -c 'cat </dev/zero | head -c 5 | tr \\0 0; echo'
+$SH -c 'cat /dev/zero | head -c 5 | tr \\0 0; echo'
+$SH -c 'cat tmp.txt | cat | head -c 5 | tr \\0 0; echo'
+
+## STDOUT:
+00000
+00000
+00000
+00000
 ## END
