6a10af57892a183faec2dd1eecac47acfc0e281c
npt-1707 <npthanh132@gmail.com>
2026-05-18 06:00:33 +0800

    Python-2.7.13/Modules/expat/xmlparse.c: Fix extraction of namespace prefix from XML name

--- Python-2.7.13/Modules/expat/xmlparse.c
+++ Python-2.7.13/Modules/expat/xmlparse.c
@@ -5436,7 +5436,7 @@ setElementTypePrefix(XML_Parser parser, ELEMENT_TYPE *elementType)
       else
         poolDiscard(&dtd->pool);
       elementType->prefix = prefix;
-
+      break;
     }
   }
   return 1;
